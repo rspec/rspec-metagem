5b2298f000fe804280ce5fec9508d5981d88814d
David Chelimsky <dchelimsky@gmail.com>
2009-06-29 17:08:22 -0400

    Spec > Rspec in mock adapter for rspec

--- lib/rspec/core/mocking/with_rspec.rb
+++ lib/rspec/core/mocking/with_rspec.rb
@@ -1,13 +1,13 @@
-require 'spec/mocks/framework'
-require 'spec/mocks/extensions'
+require 'rspec/mocks/framework'
+require 'rspec/mocks/extensions'
 
 module Rspec
   module Core
     module Mocking
       module WithRspec
-        include Spec::Mocks::ExampleMethods
+        include Rspec::Mocks::ExampleMethods
         def _setup_mocks
-          $rspec_mocks ||= Spec::Mocks::Space.new
+          $rspec_mocks ||= Rspec::Mocks::Space.new
         end
         def _verify_mocks
           $rspec_mocks.verify_all
