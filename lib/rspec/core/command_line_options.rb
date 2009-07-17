7df0173926e2de6f690cd98c141ef3b9a4022e2f
Chad Humphries <chad@spicycode.com>
2009-07-17 14:03:04 -0400

    WIP CLI options for the runner

--- lib/rspec/core/command_line_options.rb
+++ lib/rspec/core/command_line_options.rb
@@ -6,36 +6,40 @@ module Rspec
 
     class CommandLineOptions
       
-      attr_reader :args, :config
+      attr_reader :args, :options, :config
       
       def self.parse(args, config)
-        cli_options = new(args, config)
+        cli_options = new(args)
         cli_options.parse
       end
 
-      def initialize(args, config)
-        @args, @config = args, config
+      def initialize(args)
+        @args = args
+        @options = {}
       end
 
       def parse
         possible_files = OptionParser.new do |opts|
           opts.banner = "Usage: rspec [options] [files or directories]"
 
-          opts.on('-c', '--[no-]color', 'Enable color in the output') do |c|
-            @config.color_enabled = c
+          opts.on('-c', '--[no-]color', '--[no-]colour', 'Enable color in the output') do |o|
+            @options[:color_enabled] = o
           end
           
-          opts.on('-f', '--formatter [FORMATTER]', 'Choose an optional formatter') do |f|
-            @config.formatter = f
+          opts.on('-f', '--formatter [FORMATTER]', 'Choose an optional formatter') do |o|
+            options[:formatter] = o
           end
 
-          opts.on('-p', '--profile', 'Enable profiling of examples with output of the top 10 slowest examples') do |p|
-            @config.profile_examples = p
+          opts.on('-p', '--profile', 'Enable profiling of examples with output of the top 10 slowest examples') do |o|
+            options[:profile_examples] = o
           end
 
+          opts.on_tail('-h', '--help', "You're looking at it.") do 
+            puts opts
+          end
         end.parse!(@args)
-
-        config.files_to_run = possible_files
+        options[:files_to_run] = possible_files
+        options
       end
 
     end
