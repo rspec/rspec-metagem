d9f2e2da649c3589ebfe4f3c727456671b0f1880
Chad Humphries <chad@spicycode.com>
2009-08-21 11:11:34 -0400

    Let's trust the load path (mirroring RSpec 1.X)

--- .treasure_map.rb
+++ .treasure_map.rb
@@ -1,22 +1,24 @@
 clear_maps
 
+@default_runner = 'clear && ruby -Ilib -Ispec'
+
 map_for(:rspec_core) do |m|
 
   m.watch 'lib', 'spec', 'example_specs'
 
-  m.add_mapping %r%example_specs/(.*)_spec\.rb% do |match|
+  m.add_mapping %r%example_specs/(.*)_spec\.rb%, :command => @default_runner  do |match|
     ["example_specs/#{match[1]}_spec.rb"]
   end
 
-  m.add_mapping %r%spec/(.*)_spec\.rb% do |match|
+  m.add_mapping %r%spec/(.*)_spec\.rb%, :command => @default_runner  do |match|
     ["spec/#{match[1]}_spec.rb"]
   end
 
-  m.add_mapping %r%spec/spec_helper\.rb% do |match|
+  m.add_mapping %r%spec/spec_helper\.rb%, :command => @default_runner  do |match|
     Dir["spec/**/*_spec.rb"]
   end
 
-  m.add_mapping %r%lib/(.*)\.rb% do |match|
+  m.add_mapping %r%lib/(.*)\.rb%, :command => @default_runner  do |match|
     examples_matching match[1], 'spec'
   end
 
