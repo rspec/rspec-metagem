b42f885b65a5cc0645576cd6d2658ae5322db724
Chad Humphries <chad@spicycode.com>
2009-08-29 10:58:09 -0400

    Updating treasure map for latest beholder

--- .treasure_map.rb
+++ .treasure_map.rb
@@ -1,24 +1,22 @@
-clear_maps
-
-@default_runner = 'clear && ruby -Ilib -Ispec'
+Beholder.runner = 'clear; ruby -Ilib -Ispec'
 
 map_for(:rspec_core) do |m|
 
   m.watch 'lib', 'spec', 'example_specs'
 
-  m.add_mapping %r%example_specs/(.*)_spec\.rb%, :command => @default_runner  do |match|
+  m.add_mapping %r%example_specs/(.*)_spec\.rb% do |match|
     ["example_specs/#{match[1]}_spec.rb"]
   end
 
-  m.add_mapping %r%spec/(.*)_spec\.rb%, :command => @default_runner  do |match|
+  m.add_mapping %r%spec/(.*)_spec\.rb% do |match|
     ["spec/#{match[1]}_spec.rb"]
   end
 
-  m.add_mapping %r%spec/spec_helper\.rb%, :command => @default_runner  do |match|
+  m.add_mapping %r%spec/spec_helper\.rb% do |match|
     Dir["spec/**/*_spec.rb"]
   end
 
-  m.add_mapping %r%lib/(.*)\.rb%, :command => @default_runner  do |match|
+  m.add_mapping %r%lib/(.*)\.rb% do |match|
     examples_matching match[1], 'spec'
   end
 
