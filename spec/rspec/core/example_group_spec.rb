b16595345ab9609df742e3facaf722934ccee60c
David Chelimsky <dchelimsky@gmail.com>
2010-01-10 10:20:49 -0600

    begin to rename/reorg specs in example_group_spec

--- spec/rspec/core/example_group_spec.rb
+++ spec/rspec/core/example_group_spec.rb
@@ -9,53 +9,61 @@ end
 
 describe Rspec::Core::ExampleGroup do
 
-  describe "describing behaviour with #describe" do
+  describe "#describe" do
 
-    example "an ArgumentError is raised if no type or description is given" do
+    it "raises an ArgumentError if no type or description is given" do
       lambda { Rspec::Core::ExampleGroup.describe() {} }.should raise_error(ArgumentError, "No arguments given.  You must a least supply a type or description")
     end
 
-    example "an ArgumentError is raised if no block is given" do
+    it "raises an ArgumentError if no block is given" do
       lambda { Rspec::Core::ExampleGroup.describe('foo') }.should raise_error(ArgumentError, "You must supply a block when calling describe")
     end
 
-    describe '#name' do
+  end
 
-      it "should expose the first parameter as name" do
-        isolate_behaviour do
-          Rspec::Core::ExampleGroup.describe("my favorite pony") { }.name.should == 'my favorite pony'
-        end
+  describe '#name' do
+
+    it "uses the first parameter as name" do
+      isolate_behaviour do
+        Rspec::Core::ExampleGroup.describe("my favorite pony") { }.name.should == 'my favorite pony'
       end
-
-      it "should call to_s on the first parameter in case it is a constant" do
-        isolate_behaviour do
-          Rspec::Core::ExampleGroup.describe(Object) { }.name.should == 'Object'
-        end
-      end
-
-      it "should be built correctly when nested" do
-        behaviour_to_test = nil
-        group = empty_example_group('test')
-        group.name.should == 'Object test'
-
-        nested_group_one = group.describe('nested one') { }
-        nested_group_one.name.should == 'Object test nested one'
-
-        nested_group_two = nested_group_one.describe('nested two') { }
-        nested_group_two.name.should == 'Object test nested one nested two'
-      end
-
     end
 
-    describe '#describes' do
+    it "accepts a constant as the first parameter" do
+      isolate_behaviour do
+        Rspec::Core::ExampleGroup.describe(Object) { }.name.should == 'Object'
+      end
+    end
 
-      it "should be the first parameter when it is a constant" do
+    it "concats nested names" do
+      behaviour_to_test = nil
+      group = empty_example_group('test')
+      group.name.should == 'Object test'
+
+      nested_group_one = group.describe('nested one') { }
+      nested_group_one.name.should == 'Object test nested one'
+
+      nested_group_two = nested_group_one.describe('nested two') { }
+      nested_group_two.name.should == 'Object test nested one nested two'
+    end
+
+  end
+
+  describe '#describes' do
+
+    context "with a constant as the first parameter" do
+
+      it "is that constant" do
         isolate_behaviour do
           Rspec::Core::ExampleGroup.describe(Object) { }.describes.should == Object
         end
       end
 
-      it "should be nil when the first parameter is a string" do
+    end
+
+    context "with a string as the first parameter" do
+
+      it "is nil" do
         isolate_behaviour do
           Rspec::Core::ExampleGroup.describe("i'm a computer") { }.describes.should be_nil
         end
@@ -63,192 +71,192 @@ describe Rspec::Core::ExampleGroup do
 
     end
 
-    describe '#description' do
+  end
 
-      it "should expose the second parameter as description" do
-        isolate_behaviour do
-          Rspec::Core::ExampleGroup.describe(Object, "my desc") { }.description.should == 'my desc'
-        end
+  describe '#description' do
+
+    it "exposes the second parameter as description" do
+      isolate_behaviour do
+        Rspec::Core::ExampleGroup.describe(Object, "my desc") { }.description.should == 'my desc'
       end
-
-      it "should allow the second parameter to be nil" do
-        isolate_behaviour do
-          Rspec::Core::ExampleGroup.describe(Object, nil) { }.description.size.should == 0
-        end
-      end
-
     end
 
-    describe '#metadata' do
-
-      it "should add the third parameter to the metadata" do
-        isolate_behaviour do
-          Rspec::Core::ExampleGroup.describe(Object, nil, 'foo' => 'bar') { }.metadata.should include({ "foo" => 'bar' })
-        end
+    it "allows the second parameter to be nil" do
+      isolate_behaviour do
+        Rspec::Core::ExampleGroup.describe(Object, nil) { }.description.size.should == 0
       end
-
-      it "should add the caller to metadata" do
-        isolate_behaviour do
-          Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:caller][4].should =~ /#{__FILE__}/
-        end
-      end
-
-      it "should add the the file_path to metadata" do
-        isolate_behaviour do
-          Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:file_path].should == __FILE__
-        end
-      end
-
-      it "should have a reader for file_path" do
-        isolate_behaviour do
-          Rspec::Core::ExampleGroup.describe(Object) { }.file_path.should == __FILE__
-        end
-      end
-
-      it "should add the line_number to metadata" do
-        isolate_behaviour do
-          Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:line_number].should == __LINE__
-        end
-      end
-
-      it "should add file path and line number metadata for arbitrarily nested describes" do
-        Rspec::Core::ExampleGroup.describe(Object) do
-          Rspec::Core::ExampleGroup.describe("foo") do
-            Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:file_path].should == __FILE__
-            Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:line_number].should == __LINE__
-          end
-        end
-
-        4.times { remove_last_describe_from_world }
-      end
-
-    end
-
-    describe "adding before, after, and around hooks" do
-
-      it "should expose the before each blocks at before_eachs" do
-        group = empty_example_group
-        group.before(:each) { 'foo' }
-        group.should have(1).before_eachs
-      end
-
-      it "should maintain the before each block order" do
-        group = empty_example_group 
-        group.before(:each) { 15 }
-        group.before(:each) { 'A' }
-        group.before(:each) { 33.5 }
-
-        group.before_eachs[0].call.should == 15
-        group.before_eachs[1].call.should == 'A'
-        group.before_eachs[2].call.should == 33.5
-      end
-
-      it "should expose the before all blocks at before_alls" do
-        group = empty_example_group
-        group.before(:all) { 'foo' }
-        group.should have(1).before_alls
-      end
-
-      it "should maintain the before all block order" do
-        group = empty_example_group 
-        group.before(:all) { 15 }
-        group.before(:all) { 'A' }
-        group.before(:all) { 33.5 }
-
-        group.before_alls[0].call.should == 15
-        group.before_alls[1].call.should == 'A'
-        group.before_alls[2].call.should == 33.5
-      end
-
-      it "should expose the after each blocks at after_eachs" do
-        group = empty_example_group
-        group.after(:each) { 'foo' }
-        group.should have(1).after_eachs
-      end
-
-      it "should maintain the after each block order" do
-        group = empty_example_group 
-        group.after(:each) { 15 }
-        group.after(:each) { 'A' }
-        group.after(:each) { 33.5 }
-
-        group.after_eachs[0].call.should == 15
-        group.after_eachs[1].call.should == 'A'
-        group.after_eachs[2].call.should == 33.5
-      end
-
-      it "should expose the after all blocks at after_alls" do
-        group = empty_example_group
-        group.after(:all) { 'foo' }
-        group.should have(1).after_alls
-      end
-
-      it "should maintain the after each block order" do
-        group = empty_example_group 
-        group.after(:all) { 15 }
-        group.after(:all) { 'A' }
-        group.after(:all) { 33.5 }
-
-        group.after_alls[0].call.should == 15
-        group.after_alls[1].call.should == 'A'
-        group.after_alls[2].call.should == 33.5
-      end
-
-      it "should expose the around each blocks at after_alls" do
-        group = empty_example_group
-        group.around(:each) { 'foo' }
-        group.should have(1).around_eachs
-      end
-      
-    end
-
-    describe "adding examples" do
-
-      it "should allow adding an example using 'it'" do
-        group = empty_example_group
-        group.it("should do something") { }
-        group.examples.size.should == 1
-      end
-
-      it "should expose all examples at examples" do
-        group = empty_example_group
-        group.it("should do something 1") { }
-        group.it("should do something 2") { }
-        group.it("should do something 3") { }
-        group.examples.size.should == 3
-      end
-
-      it "should maintain the example order" do
-        group = empty_example_group
-        group.it("should 1") { }
-        group.it("should 2") { }
-        group.it("should 3") { }
-        group.examples[0].description.should == 'should 1'
-        group.examples[1].description.should == 'should 2'
-        group.examples[2].description.should == 'should 3'
-      end
-
     end
 
   end
 
-  describe Object, "describing nested behaviours", :little_less_nested => 'yep' do 
+  describe '#metadata' do
 
-    describe "A sample nested describe", :nested_describe => "yep" do
+    it "adds the third parameter to the metadata" do
+      isolate_behaviour do
+        Rspec::Core::ExampleGroup.describe(Object, nil, 'foo' => 'bar') { }.metadata.should include({ "foo" => 'bar' })
+      end
+    end
 
-      it "should set the described type to the constant Object" do
+    it "adds the caller to metadata" do
+      isolate_behaviour do
+        Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:caller][4].should =~ /#{__FILE__}/
+      end
+    end
+
+    it "adds the the file_path to metadata" do
+      isolate_behaviour do
+        Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:file_path].should == __FILE__
+      end
+    end
+
+    it "has a reader for file_path" do
+      isolate_behaviour do
+        Rspec::Core::ExampleGroup.describe(Object) { }.file_path.should == __FILE__
+      end
+    end
+
+    it "adds the line_number to metadata" do
+      isolate_behaviour do
+        Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:line_number].should == __LINE__
+      end
+    end
+
+    it "adds file path and line number metadata for arbitrarily nested describes" do
+      Rspec::Core::ExampleGroup.describe(Object) do
+        Rspec::Core::ExampleGroup.describe("foo") do
+          Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:file_path].should == __FILE__
+          Rspec::Core::ExampleGroup.describe(Object) { }.metadata[:behaviour][:line_number].should == __LINE__
+        end
+      end
+
+      4.times { remove_last_describe_from_world }
+    end
+
+  end
+
+  describe "adding before, after, and around hooks" do
+
+    it "should expose the before each blocks at before_eachs" do
+      group = empty_example_group
+      group.before(:each) { 'foo' }
+      group.should have(1).before_eachs
+    end
+
+    it "should maintain the before each block order" do
+      group = empty_example_group 
+      group.before(:each) { 15 }
+      group.before(:each) { 'A' }
+      group.before(:each) { 33.5 }
+
+      group.before_eachs[0].call.should == 15
+      group.before_eachs[1].call.should == 'A'
+      group.before_eachs[2].call.should == 33.5
+    end
+
+    it "should expose the before all blocks at before_alls" do
+      group = empty_example_group
+      group.before(:all) { 'foo' }
+      group.should have(1).before_alls
+    end
+
+    it "should maintain the before all block order" do
+      group = empty_example_group 
+      group.before(:all) { 15 }
+      group.before(:all) { 'A' }
+      group.before(:all) { 33.5 }
+
+      group.before_alls[0].call.should == 15
+      group.before_alls[1].call.should == 'A'
+      group.before_alls[2].call.should == 33.5
+    end
+
+    it "should expose the after each blocks at after_eachs" do
+      group = empty_example_group
+      group.after(:each) { 'foo' }
+      group.should have(1).after_eachs
+    end
+
+    it "should maintain the after each block order" do
+      group = empty_example_group 
+      group.after(:each) { 15 }
+      group.after(:each) { 'A' }
+      group.after(:each) { 33.5 }
+
+      group.after_eachs[0].call.should == 15
+      group.after_eachs[1].call.should == 'A'
+      group.after_eachs[2].call.should == 33.5
+    end
+
+    it "should expose the after all blocks at after_alls" do
+      group = empty_example_group
+      group.after(:all) { 'foo' }
+      group.should have(1).after_alls
+    end
+
+    it "should maintain the after each block order" do
+      group = empty_example_group 
+      group.after(:all) { 15 }
+      group.after(:all) { 'A' }
+      group.after(:all) { 33.5 }
+
+      group.after_alls[0].call.should == 15
+      group.after_alls[1].call.should == 'A'
+      group.after_alls[2].call.should == 33.5
+    end
+
+    it "should expose the around each blocks at after_alls" do
+      group = empty_example_group
+      group.around(:each) { 'foo' }
+      group.should have(1).around_eachs
+    end
+    
+  end
+
+  describe "adding examples" do
+
+    it "should allow adding an example using 'it'" do
+      group = empty_example_group
+      group.it("should do something") { }
+      group.examples.size.should == 1
+    end
+
+    it "should expose all examples at examples" do
+      group = empty_example_group
+      group.it("should do something 1") { }
+      group.it("should do something 2") { }
+      group.it("should do something 3") { }
+      group.examples.size.should == 3
+    end
+
+    it "should maintain the example order" do
+      group = empty_example_group
+      group.it("should 1") { }
+      group.it("should 2") { }
+      group.it("should 3") { }
+      group.examples[0].description.should == 'should 1'
+      group.examples[1].description.should == 'should 2'
+      group.examples[2].description.should == 'should 3'
+    end
+
+  end
+
+  describe Object, "describing nested example_groups", :little_less_nested => 'yep' do 
+
+    describe "A sample nested group", :nested_describe => "yep" do
+
+      it "sets the described class to the constant Object" do
         running_example.behaviour.describes.should == Object
       end
 
-      it "should set the description to 'A sample nested describe'" do
-        running_example.behaviour.description.should == 'A sample nested describe'
+      it "sets the description to 'A sample nested describe'" do
+        running_example.behaviour.description.should == 'A sample nested group'
       end
 
-      it "should have top level metadata from the behaviour and its ancestors" do
+      it "has top level metadata from the behaviour and its ancestors" do
         running_example.behaviour.metadata.should include(:little_less_nested => 'yep', :nested_describe => 'yep')
       end
 
-      it "should make the parent metadata available on the contained examples" do
+      it "exposes the parent metadata to the contained examples" do
         running_example.metadata.should include(:little_less_nested => 'yep', :nested_describe => 'yep')
       end
 
@@ -346,7 +354,7 @@ describe Rspec::Core::ExampleGroup do
 
       describe "accessing a before_all ivar that was changed in a parent behaviour" do
 
-        it "should have access to the modified version" do
+        it "has access to the modified version" do
           @before_all_top_level.should == 'ive been changed'
         end
 
