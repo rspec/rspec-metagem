d3907289a1d324f2e57fa192f20f5138b85bde0b
David Chelimsky <dchelimsky@gmail.com>
2010-01-26 01:42:23 -0600

    a little cleaner

--- lib/rspec/core/configuration.rb
+++ lib/rspec/core/configuration.rb
@@ -137,16 +137,15 @@ module Rspec
       end
       
       def files_or_directories_to_run=(*files)
-        p files
         options[:files_to_run] = files.flatten.inject([]) do |result, file|
           if File.directory?(file)
             filename_pattern.split(",").each do |pattern|
               result += Dir[File.expand_path("#{file}/#{pattern.strip}")]
             end
           else
-            file =~ /([^:]*):?(\d+)?/
-            self.line_number = $2 if $2
-            result << $1
+            path, line_number = file.split(':')
+            self.line_number = line_number if line_number
+            result << path
           end
           result
         end
