c550dc838e09d9dc4da71edffda1caf3f3b1febf
David Chelimsky <dchelimsky@gmail.com>
2010-03-01 22:22:52 -0600

    use local rspec command in cukes

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,16 @@
-require 'aruba'
+$LOAD_PATH << File.expand_path("../../../../rspec-expectations/lib", __FILE__)
 require 'rspec/expectations'
+require 'aruba'
+
+module ArubaOverrides
+  def detect_ruby_script(cmd)
+    if cmd =~ /^rspec /
+      "ruby -I../../lib -S ../../bin/#{cmd}"
+    else
+      super(cmd)
+    end
+  end
+end
+
+World(ArubaOverrides)
+
