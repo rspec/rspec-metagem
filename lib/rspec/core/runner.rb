86aa0ccbfc638ec63050b29ee538ff85becd9039
David Chelimsky <dchelimsky@gmail.com>
2010-07-06 20:39:45 -0500

    Add means of disabling autorun.

--- lib/rspec/core/runner.rb
+++ lib/rspec/core/runner.rb
@@ -4,14 +4,18 @@ module RSpec
   module Core
     class Runner
 
-      def self.installed_at_exit?
-        @installed_at_exit ||= false
-      end
-
       def self.autorun
-        return if installed_at_exit? || running_in_drb?
+        return if autorun_disabled? || installed_at_exit? || running_in_drb?
         @installed_at_exit = true
         at_exit { run(ARGV, $stderr, $stdout) ? exit(0) : exit(1) }
+      end
+
+      def self.autorun_disabled?
+        !!ENV['DISABLE_RSPEC_AUTORUN']
+      end
+
+      def self.installed_at_exit?
+        @installed_at_exit ||= false
       end
 
       def self.running_in_drb?
