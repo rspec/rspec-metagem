3f55e712f9ce64548cf4046a45f650dd88b2db6e
Ben Armston <ben.armston@googlemail.com>
2010-07-08 00:33:41 +0100

    Attempt to move around hook wrapping to hooks file
    ExampleGroup delegates the running of before and after hooks to the Hooks
    module. This is an aborted attempt to do the same for around hooks.
    
    My intention was to have an AroundHook take a wrapped example and further
    itself around it and return the result. Whilst this had an element of
    increased consistency, it didn't make the code any easier to follow.
    
    As the before/after hook refactoring may be desirable, I'm committing those
    changes only.

--- lib/rspec/core/hooks.rb
+++ lib/rspec/core/hooks.rb
@@ -1,7 +1,8 @@
 module RSpec
   module Core
     module Hooks
-      class Hook
+
+      class HookBase
         attr_reader :options
 
         def initialize(options, block)
@@ -13,12 +14,24 @@ module RSpec
           !group || group.all_apply?(options)
         end
 
+        def to_proc
+          @block
+        end
+      end
+
+      class Hook < HookBase
         def call
           @block.call
         end
 
-        def to_proc
-          @block
+        def run_in(example)
+          example ? example.instance_eval(&self) : call
+        end
+      end
+
+      class AroundHook < HookBase
+        def call(wrapped_example)
+          @block.call(wrapped_example)
         end
       end
 
@@ -38,17 +51,17 @@ module RSpec
         hooks[:after][scope] << Hook.new(options, block)
       end
 
-      def around(scope=:each, &block)
-        hooks[:around][scope] << block
+      def around(scope=:each, options={}, &block)
+        hooks[:around][scope] << AroundHook.new(options, block)
       end
 
       # Runs all of the blocks stored with the hook in the context of the
       # example. If no example is provided, just calls the hook directly.
       def run_hook(hook, scope, example=nil, options={})
         if options[:reverse]
-          hooks[hook][scope].reverse.each &run_hook_in(example)
+          hooks[hook][scope].reverse.each {|h| h.run_in(example) }
         else
-          hooks[hook][scope].each &run_hook_in(example)
+          hooks[hook][scope].each {|h| h.run_in(example) }
         end
       end
 
@@ -57,25 +70,19 @@ module RSpec
       def run_hook!(hook, scope, example, options={})
         until hooks[hook][scope].empty?
           if options[:reverse] 
-            example.instance_eval &hooks[hook][scope].shift
+            hooks[hook][scope].shift.run_in(example)
           else
-            example.instance_eval &hooks[hook][scope].pop
+            hooks[hook][scope].pop.run_in(example)
           end
         end
       end
 
       def run_hook_filtered(hook, scope, group, example)
-        find_hook(hook, scope, group).each &run_hook_in(example)
+        find_hook(hook, scope, group).each {|h| h.run_in(example) }
       end
 
       def find_hook(hook, scope, group)
         hooks[hook][scope].select {|hook| hook.options_apply?(group)}
-      end
-
-    private
-
-      def run_hook_in(example)
-        lambda {|hook| example ? example.instance_eval(&hook) : hook.call}
       end
     end
   end
