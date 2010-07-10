d4a7cd8e0be44a7e3beda69a9ffabab49ac7b172
David Chelimsky <dchelimsky@gmail.com>
2010-07-10 08:20:09 -0500

    no need to override aruba's detection of ruby script now

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,15 +1,2 @@
 require 'aruba'
 require 'rspec/expectations'
-
-module ArubaOverrides
-  def detect_ruby_script(cmd)
-    if cmd =~ /^rspec /
-      "../../bin/#{cmd}"
-    else
-      super(cmd)
-    end
-  end
-end
-
-World(ArubaOverrides)
-
