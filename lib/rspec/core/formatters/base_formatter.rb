91144dc0fc9b6dc21cfde830f5a711fedfa8df96
David Chelimsky <dchelimsky@gmail.com>
2010-09-22 23:59:22 -0500

    Attempt #2 at the previous refactoring.

--- lib/rspec/core/formatters/base_formatter.rb
+++ lib/rspec/core/formatters/base_formatter.rb
@@ -119,21 +119,25 @@ module RSpec
         end
 
         def read_failed_line(exception, example)
-          original_file = example.file_path.to_s.downcase
-
-          matching_line = exception.backtrace.detect { |line|
-            match = line.match(/(.+?):(\d+)(|:\d+)/)
-            match && match[1].downcase == original_file.downcase
-          }
-
-          return "Unable to find matching line from backtrace" if matching_line.nil?
+          unless matching_line = find_failed_line(exception.backtrace, example.file_path)
+            return "Unable to find matching line from backtrace"
+          end
 
           file_path, line_number = matching_line.match(/(.+?):(\d+)(|:\d+)/)[1..2]
+
           if File.exist?(file_path)
             open(file_path, 'r') { |f| f.readlines[line_number.to_i - 1] }
           else
             "Unable to find #{file_path} to read failed line"
           end
+        end
+
+        def find_failed_line(backtrace, path)
+          backtrace.detect { |line|
+            match = line.match(/(.+?):(\d+)(|:\d+)/)
+            match && match[1].downcase == path.downcase
+          }
+
         end
 
         def start_sync_output
