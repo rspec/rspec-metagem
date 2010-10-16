7852dbd08d83d0e09c9481dda2372ee71d29a363
Myron Marston <myron.marston@gmail.com>
2010-10-15 18:28:32 -0700

    Add be_within(delta).of(expected) matcher
    - Delegate be_close(expected, delta) to be_within
    - Deprecate be_close(expected, delta)
    - Reads much better.
    - The argument ordering is clearer.
    - Closes #32.

--- lib/rspec/matchers/be_close.rb
+++ lib/rspec/matchers/be_close.rb
@@ -10,23 +10,8 @@ module RSpec
     #
     #   result.should be_close(3.0, 0.5)
     def be_close(expected, delta)
-      Matcher.new :be_close, expected, delta do |_expected_, _delta_|
-        match do |actual|
-          (actual - _expected_).abs < _delta_
-        end
-
-        failure_message_for_should do |actual|
-          "expected #{_expected_} +/- (< #{_delta_}), got #{actual}"
-        end
-
-        failure_message_for_should_not do |actual|
-          "expected #{_expected_} +/- (< #{_delta_}), got #{actual}"
-        end
-
-        description do
-          "be close to #{_expected_} (within +- #{_delta_})"
-        end
-      end
+      RSpec.deprecate("be_close(#{expected}, #{delta})", "be_within(#{delta}).of(#{expected})")
+      be_within(delta).of(expected)
     end
   end
 end
