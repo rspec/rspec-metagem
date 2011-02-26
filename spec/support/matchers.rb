da49bee71428f1b3804c64dc8622c7b0d1b918af
Myron Marston <myron.marston@gmail.com>
2011-02-26 11:53:51 -0800

    Move custom matchers into spec/support/matchers.rb.

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -19,3 +19,53 @@ RSpec::Matchers.define :map_specs do |specs|
     autotest
   end
 end
+
+RSpec::Matchers.define :be_pending_with do |message|
+  match do |example|
+    example.metadata[:pending] && example.metadata[:execution_result][:pending_message] == message
+  end
+
+  failure_message_for_should do |example|
+    "expected example to pending with #{message.inspect}, got #{example.metadata[:execution_result][:pending_message].inspect}"
+  end
+end
+
+RSpec::Matchers.define :fail_with do |exception_klass|
+  match do |example|
+    failure_reason(example, exception_klass).nil?
+  end
+
+  failure_message_for_should do |example|
+    "expected example to fail with a #{exception_klass} exception, but #{failure_reason(example, exception_klass)}"
+  end
+
+  def failure_reason(example, exception_klass)
+    result = example.metadata[:execution_result]
+    case
+      when example.metadata[:pending] then "was pending"
+      when result[:status] != 'failed' then result[:status]
+      when !result[:exception].is_a?(exception_klass) then "failed with a #{result[:exception].class}"
+      else nil
+    end
+  end
+end
+
+RSpec::Matchers.define :pass do
+  match do |example|
+    failure_reason(example).nil?
+  end
+
+  failure_message_for_should do |example|
+    "expected example to pass, but #{failure_reason(example)}"
+  end
+
+  def failure_reason(example)
+    result = example.metadata[:execution_result]
+    case
+      when example.metadata[:pending] then "was pending"
+      when result[:status] != 'passed' then result[:status]
+      else nil
+    end
+  end
+end
+
