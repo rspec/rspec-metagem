96ca109bf7e0085b31b699584a9c35fb7e6fda21
Myron Marston <myron.marston@gmail.com>
2011-03-28 10:03:28 -0700

    Use RUBY_PLATFORM rather than RUBY_ENGINE to detect jruby.
    RUBY_ENGINE is not defined when using MRI.  RUBY_PLATFORM is.

--- spec/rspec/core/formatters/base_formatter_spec.rb
+++ spec/rspec/core/formatters/base_formatter_spec.rb
@@ -46,7 +46,7 @@ describe RSpec::Core::Formatters::BaseFormatter do
       end
 
       it "doesn't hang when file exists" do
-        pending("This issue still exists on JRuby, but should be resolved shortly: https://github.com/rspec/rspec-core/issues/295", :if => RUBY_ENGINE == 'jruby')
+        pending("This issue still exists on JRuby, but should be resolved shortly: https://github.com/rspec/rspec-core/issues/295", :if => RUBY_PLATFORM == 'java')
         exception = mock(:Exception, :backtrace => [ "#{__FILE__}:#{__LINE__}"])
 
         example = mock(:Example, :file_path => __FILE__)
