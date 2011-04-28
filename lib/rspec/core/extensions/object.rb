86a0813f00334dbc3f10dbc8f6bc036e54cae0c0
David Chelimsky <dchelimsky@gmail.com>
2011-04-28 07:19:55 -0400

    remove redundant statement

--- lib/rspec/core/extensions/object.rb
+++ lib/rspec/core/extensions/object.rb
@@ -2,7 +2,6 @@ module RSpec
   module Core
     module ObjectExtensions
       def describe(*args, &example_group_block)
-        args << {} unless args.last.is_a?(Hash)
         RSpec::Core::ExampleGroup.describe(*args, &example_group_block).register
       end
     end
