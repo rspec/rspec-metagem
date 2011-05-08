1466f60418c51440c21c919b930fdd6cff95b0c5
David Chelimsky <dchelimsky@gmail.com>
2011-05-08 10:52:04 -0500

    add comment to remove spork monkey patch

--- lib/rspec/monkey/spork/test_framework/rspec.rb
+++ lib/rspec/monkey/spork/test_framework/rspec.rb
@@ -1,3 +1,4 @@
+# TODO (2011-05-08) - remove this as soon as spork 0.9.0 is released
 if defined?(Spork::TestFramework::RSpec)
   class Spork::TestFramework::RSpec < Spork::TestFramework
     def run_tests(argv, err, out)
