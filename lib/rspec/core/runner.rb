a0d31128b89f5a068a7d0106bf766213449425c5
David Chelimsky <dchelimsky@gmail.com>
2011-06-24 07:18:03 -0500

    Revert "Make sure nested at_exit blocks don't break exit status"
    This reverts commit c24caf637a67204e130d5fb51353acd8361c5b58.

--- lib/rspec/core/runner.rb
+++ lib/rspec/core/runner.rb
@@ -8,12 +8,7 @@ module RSpec
       def self.autorun
         return if autorun_disabled? || installed_at_exit? || running_in_drb?
         @installed_at_exit = true
-        at_exit do
-          exit_status = 0
-          # make sure this one really runs last to ensure proper exit status
-          at_exit { exit(exit_status) }
-          exit_status = run(ARGV, $stderr, $stdout).to_i
-        end
+        at_exit { exit(run(ARGV, $stderr, $stdout).to_i) }
       end
       AT_EXIT_HOOK_BACKTRACE_LINE = "#{__FILE__}:#{__LINE__ - 2}:in `autorun'"
 
