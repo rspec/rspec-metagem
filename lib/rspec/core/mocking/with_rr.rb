b4d826784ab3e4fe521a26e868c2475a75ac0d31
Nick Novitski <nicknovitski@gmail.com>
2011-08-21 11:06:55 -0700

    use RR's rspec2 adapter (re: issue #136)

--- lib/rspec/core/mocking/with_rr.rb
+++ lib/rspec/core/mocking/with_rr.rb
@@ -5,23 +5,10 @@ RSpec.configuration.backtrace_clean_patterns.push(RR::Errors::BACKTRACE_IDENTIFI
 module RSpec
   module Core
     module MockFrameworkAdapter
-      
+
       def self.framework_name; :rr end
 
-      include RR::Extensions::InstanceMethods
-
-      def setup_mocks_for_rspec
-        RR::Space.instance.reset
-      end
-
-      def verify_mocks_for_rspec
-        RR::Space.instance.verify_doubles
-      end
-
-      def teardown_mocks_for_rspec
-        RR::Space.instance.reset
-      end
-
+      include RR::Adapters::RSpec2
     end
   end
 end
