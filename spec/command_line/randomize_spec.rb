097331905898e8f1f702c8ee8b02e64036761bda
David Chelimsky <dchelimsky@gmail.com>
2011-10-23 11:26:43 -0400

    refactor randomize_spec

--- spec/command_line/randomize_spec.rb
+++ spec/command_line/randomize_spec.rb
@@ -9,21 +9,6 @@ describe 'command line' do
         specify('group 1 example 3')  {}
         specify('group 1 example 4')  {}
         specify('group 1 example 5')  {}
-        specify('group 1 example 6')  {}
-        specify('group 1 example 7')  {}
-        specify('group 1 example 8')  {}
-        specify('group 1 example 9')  {}
-        specify('group 1 example 10') {}
-        specify('group 1 example 11') {}
-        specify('group 1 example 12') {}
-        specify('group 1 example 13') {}
-        specify('group 1 example 14') {}
-        specify('group 1 example 15') {}
-        specify('group 1 example 16') {}
-        specify('group 1 example 17') {}
-        specify('group 1 example 18') {}
-        specify('group 1 example 19') {}
-        specify('group 1 example 20') {}
 
         describe 'group 1-1' do
           specify('group 1-1 example 1')  {}
@@ -31,97 +16,29 @@ describe 'command line' do
           specify('group 1-1 example 3')  {}
           specify('group 1-1 example 4')  {}
           specify('group 1-1 example 5')  {}
-          specify('group 1-1 example 6')  {}
-          specify('group 1-1 example 7')  {}
-          specify('group 1-1 example 8')  {}
-          specify('group 1-1 example 9')  {}
-          specify('group 1-1 example 10') {}
-          specify('group 1-1 example 11') {}
-          specify('group 1-1 example 12') {}
-          specify('group 1-1 example 13') {}
-          specify('group 1-1 example 14') {}
-          specify('group 1-1 example 15') {}
-          specify('group 1-1 example 16') {}
-          specify('group 1-1 example 17') {}
-          specify('group 1-1 example 18') {}
-          specify('group 1-1 example 19') {}
-          specify('group 1-1 example 20') {}
         end
 
         describe('group 1-2')  { specify('example') {} }
         describe('group 1-3')  { specify('example') {} }
         describe('group 1-4')  { specify('example') {} }
         describe('group 1-5')  { specify('example') {} }
-        describe('group 1-6')  { specify('example') {} }
-        describe('group 1-7')  { specify('example') {} }
-        describe('group 1-8')  { specify('example') {} }
-        describe('group 1-9')  { specify('example') {} }
-        describe('group 1-10') { specify('example') {} }
-        describe('group 1-11') { specify('example') {} }
-        describe('group 1-12') { specify('example') {} }
-        describe('group 1-13') { specify('example') {} }
-        describe('group 1-14') { specify('example') {} }
-        describe('group 1-15') { specify('example') {} }
-        describe('group 1-16') { specify('example') {} }
-        describe('group 1-17') { specify('example') {} }
-        describe('group 1-18') { specify('example') {} }
-        describe('group 1-19') { specify('example') {} }
-        describe('group 1-20') { specify('example') {} }
       end
 
       describe('group 2')  { specify('example') {} }
       describe('group 3')  { specify('example') {} }
       describe('group 4')  { specify('example') {} }
       describe('group 5')  { specify('example') {} }
-      describe('group 6')  { specify('example') {} }
-      describe('group 7')  { specify('example') {} }
-      describe('group 8')  { specify('example') {} }
-      describe('group 9')  { specify('example') {} }
-      describe('group 10') { specify('example') {} }
-      describe('group 11') { specify('example') {} }
-      describe('group 12') { specify('example') {} }
-      describe('group 13') { specify('example') {} }
-      describe('group 14') { specify('example') {} }
-      describe('group 15') { specify('example') {} }
-      describe('group 16') { specify('example') {} }
-      describe('group 17') { specify('example') {} }
-      describe('group 18') { specify('example') {} }
-      describe('group 19') { specify('example') {} }
-      describe('group 20') { specify('example') {} }
     """
   end
 
-  def assert_randomized(array)
-    split_in_two(array)[0].should_not eq(split_in_two(array)[1])
-  end
-
-  def assert_not_randomized(array)
-    split_in_two(array)[0].should eq(split_in_two(array)[1])
-  end
-
-  def split_in_two(array)
-    length, half = array.length, array.length / 2
-    [array.slice(0, half), array.slice(half, length)]
-  end
-
-  def example_groups(level)
-    all_stdout.scan(/^\s{#{level}}group.*$/)
-  end
-
-  def examples(group)
-    all_stdout.scan(/^\s+#{group} example.*$/)
-  end
-
   describe '--randomize' do
-    it 'runs the example groups and examples in random order' do
-      2.times do
-        run_simple 'rspec spec/randomize_spec.rb --randomize -f doc', false
-      end
+    it 'runs the examples and groups in a different order each time' do
+      2.times { run_command 'rspec spec/randomize_spec.rb --randomize -f doc' }
 
-      assert_randomized example_groups(0)
-      assert_randomized example_groups(2)
-      assert_randomized examples('group 1')
-      assert_randomized examples('group 1-1')
+      top_level_groups      {|first_run, second_run| first_run.should_not eq(second_run)}
+      nested_groups         {|first_run, second_run| first_run.should_not eq(second_run)}
+      examples('group 1')   {|first_run, second_run| first_run.should_not eq(second_run)}
+      examples('group 1-1') {|first_run, second_run| first_run.should_not eq(second_run)}
 
       all_stdout.should match(
         /This run was randomized by the following seed: \d+/
@@ -130,19 +47,43 @@ describe 'command line' do
   end
 
   describe '--seed' do
-    it 'runs the example groups and examples in the same order' do
-      2.times do
-        run_simple 'rspec spec/randomize_spec.rb --seed 123 -f doc', false
-      end
+    it 'runs the examples and groups in the same order each time' do
+      2.times { run_command 'rspec spec/randomize_spec.rb --seed 123 -f doc' }
 
-      assert_not_randomized example_groups(0)
-      assert_not_randomized example_groups(2)
-      assert_not_randomized examples('group 1')
-      assert_not_randomized examples('group 1-1')
+      top_level_groups      {|first_run, second_run| first_run.should eq(second_run)}
+      nested_groups         {|first_run, second_run| first_run.should eq(second_run)}
+      examples('group 1')   {|first_run, second_run| first_run.should eq(second_run)}
+      examples('group 1-1') {|first_run, second_run| first_run.should eq(second_run)}
 
       all_stdout.should match(
         /This run was randomized by the following seed: 123/
       )
     end
+  end
+
+  def examples(group)
+    yield split_in_half(all_stdout.scan(/^\s+#{group} example.*$/))
+  end
+
+  def top_level_groups
+    yield example_groups_at_level(0)
+  end
+
+  def nested_groups
+    yield example_groups_at_level(2)
+  end
+
+  def example_groups_at_level(level)
+    split_in_half(all_stdout.scan(/^\s{#{level*2}}group.*$/))
+  end
+
+  def split_in_half(array)
+    length, midpoint = array.length, array.length / 2
+    return array.slice(0, midpoint), array.slice(midpoint, length)
+  end
+
+  def run_command(cmd)
+    # Wraps aruba api - 2nd param is fail_on_error
+    run_simple cmd, false
   end
 end
