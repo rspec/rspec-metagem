3b996ae9ca80212061ba71b1f526b6e73f7c568f
David Chelimsky <dchelimsky@gmail.com>
2011-11-25 04:58:54 -0600

    restore debugger method directly to Kernel

--- lib/rspec/core/extensions/kernel.rb
+++ lib/rspec/core/extensions/kernel.rb
@@ -1,18 +1,9 @@
-module RSpec::Core::Extensions
-  # `Kernel` gets extended with this unless `Kernel#debugger` is already
-  # defined.
-  module KernelExtensions
-
+module Kernel
+  unless defined?(debugger)
     # If not already defined by ruby-debug, this implementation prints helpful
     # message to STDERR when ruby-debug is not loaded.
     def debugger(*args)
       (RSpec.configuration.error_stream || $stderr).puts "\n***** debugger statement ignored, use -d or --debug option to enable debugging\n#{caller(0)[1]}"
     end 
-  end
-end
-
-Kernel.module_eval do
-  unless respond_to?(:debugger)
-    extend RSpec::Core::Extensions::KernelExtensions
   end
 end
