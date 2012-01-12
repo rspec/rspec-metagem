7ce078e4948e8f0d1745a50bb83dd87a68b2e50e
David Chelimsky <dchelimsky@gmail.com>
2012-01-12 08:06:20 -0600

    CONDITIONAL is more descriptive

--- lib/rspec/core/drb_options.rb
+++ lib/rspec/core/drb_options.rb
@@ -52,11 +52,11 @@ module RSpec::Core
       end
     end
 
-    BOOLEAN_FILTERS = [:if, :unless].to_set
+    CONDITIONAL_FILTERS = [:if, :unless].to_set
 
     def add_filter(argv, name, hash)
       hash.each_pair do |k, v|
-        next if BOOLEAN_FILTERS.include?(k)
+        next if CONDITIONAL_FILTERS.include?(k)
         tag = name == :inclusion ? k.to_s : "~#{k}"
         tag << ":#{v}" if v.is_a?(String)
         argv << "--tag" << tag
