21752df73e712c5c0f85ece3474736afe2be4b27
mirasrael <d_signer@mail.ru>
2012-03-31 09:10:31 +0400

    Fix RSpec::Matchers::DSL#define for compatibility with 1.8.x

--- lib/rspec/matchers/dsl.rb
+++ lib/rspec/matchers/dsl.rb
@@ -4,9 +4,11 @@ module RSpec
       # Defines a custom matcher.
       # @see RSpec::Matchers
       def define(name, &declarations)
-        matcher = RSpec::Matchers::DSL::Matcher.new(name, &declarations)
+        template_matcher = RSpec::Matchers::DSL::Matcher.new(name, &declarations)
         define_method name do |*expected|
-          matcher.for_expected(*expected).tap { |m| m.matcher_execution_context = @matcher_execution_context || self }
+          matcher = template_matcher.for_expected(*expected)
+          matcher.matcher_execution_context = @matcher_execution_context || self
+          matcher
         end
       end
 
