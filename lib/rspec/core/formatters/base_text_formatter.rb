a1cae3632c37e7261fb944a0a8dd5fb911c05b7f
Justin Ko <jko170@gmail.com>
2012-04-18 04:13:55 -0600

    line break [ci skip]

--- lib/rspec/core/formatters/base_text_formatter.rb
+++ lib/rspec/core/formatters/base_text_formatter.rb
@@ -57,6 +57,7 @@ module RSpec
 
           total, slows = [examples, sorted_examples].map {|exs|
             exs.inject(0.0) {|i, e| i + e.execution_result[:run_time] }}
+
           output.puts "\nTop #{sorted_examples.size} slowest examples (#{format_seconds(slows)} seconds, #{'%.1f' % ((slows / total) * 100)}% of total time):\n"
 
           sorted_examples.each do |example|
