8567ac6fdab91f9c83523e7278877ea31ee550b4
Myron Marston <myron.marston@gmail.com>
2012-05-11 10:45:23 -0700

    Cleanup code a bit.
    - Remove bang from method...as @justinko rightly pointed out, there's no corresponding bangless method so it didn't really follow convention here.
    - Use an early guarded return.

--- lib/rspec/expectations/expectation_target.rb
+++ lib/rspec/expectations/expectation_target.rb
@@ -6,24 +6,23 @@ module RSpec
       end
 
       def to(matcher=nil, message=nil, &block)
-        prevent_operator_matchers!(:to, matcher)
+        prevent_operator_matchers(:to, matcher)
         RSpec::Expectations::PositiveExpectationHandler.handle_matcher(@target, matcher, message, &block)
       end
 
       def to_not(matcher=nil, message=nil, &block)
-        prevent_operator_matchers!(:to_not, matcher)
+        prevent_operator_matchers(:to_not, matcher)
         RSpec::Expectations::NegativeExpectationHandler.handle_matcher(@target, matcher, message, &block)
       end
       alias not_to to_not
 
     private
 
-      def prevent_operator_matchers!(verb, matcher)
-        unless matcher
-          raise ArgumentError, "The expect syntax does not support operator matchers, " +
-                               "so you must pass a matcher to `##{verb}`."
-        end
+      def prevent_operator_matchers(verb, matcher)
+        return if matcher
 
+        raise ArgumentError, "The expect syntax does not support operator matchers, " +
+                             "so you must pass a matcher to `##{verb}`."
       end
     end
   end
