38b247a5068b530a7578cfc7809ce2082ce731db
David Chelimsky <dchelimsky@gmail.com>
2012-05-21 08:19:43 -0500

    refactor BeWithin to be more self-contained.
    More explicit and consistent in dealing with instance variables rather
    than relying on knowledge of how BaseMatcher handles them.

--- lib/rspec/matchers/built_in/be_within.rb
+++ lib/rspec/matchers/built_in/be_within.rb
@@ -4,17 +4,15 @@ module RSpec
       class BeWithin
         include BaseMatcher
 
-        attr_reader :delta
-
         def initialize(delta)
           @delta = delta
         end
 
         def matches?(actual)
-          check_presence_of_expected
-          check_actual_is_numeric(actual)
-
-          (super(actual) - expected).abs <= delta
+          @actual = actual
+          raise needs_expected unless defined? @expected 
+          raise needs_numeric  unless @actual.is_a? Numeric 
+          (@actual - @expected).abs <= @delta
         end
 
         def of(expected)
@@ -23,29 +21,25 @@ module RSpec
         end
 
         def failure_message_for_should
-          "expected #{actual} to #{description}"
+          "expected #{@actual} to #{description}"
         end
 
         def failure_message_for_should_not
-          "expected #{actual} not to #{description}"
+          "expected #{@actual} not to #{description}"
         end
 
         def description
-          "be within #{delta} of #{expected}"
+          "be within #{@delta} of #{@expected}"
         end
 
         private
 
-        def check_presence_of_expected
-          unless defined?(@expected)
-            raise ArgumentError.new("You must set an expected value using #of: be_within(#{delta}).of(expected_value)")
-          end
+        def needs_numeric
+          ArgumentError.new "The actual value (#{@actual.inspect}) must be of a `Numeric` type"
         end
 
-        def check_actual_is_numeric(actual)
-          unless actual.is_a? Numeric
-            raise ArgumentError, "The actual value (#{actual.inspect}) must be of a `Numeric` type"
-          end
+        def needs_expected
+          ArgumentError.new "You must set an expected value using #of: be_within(#{@delta}).of(expected_value)"
         end
       end
     end
