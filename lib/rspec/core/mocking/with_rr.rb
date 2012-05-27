5090cc334e2db6eefa3a6fc215f2d7d77eb0b264
Justin Ko <jko170@gmail.com>
2012-05-27 03:31:42 -0600

    Revert "Merge pull request #443 from nicknovitski/patch-2"
    This reverts commit ea668aa62e127adcb0534496834866f54f0ae65f, reversing
    changes made to 8010fc328fbb203f1fbcda4914e478a66f2467bc.

--- lib/rspec/core/mocking/with_rr.rb
+++ lib/rspec/core/mocking/with_rr.rb
@@ -5,10 +5,23 @@ RSpec.configuration.backtrace_clean_patterns.push(RR::Errors::BACKTRACE_IDENTIFI
 module RSpec
   module Core
     module MockFrameworkAdapter
-
+      
       def self.framework_name; :rr end
 
-      include RR::Adapters::RSpec2
+      include RR::Extensions::InstanceMethods
+
+      def setup_mocks_for_rspec
+        RR::Space.instance.reset
+      end
+
+      def verify_mocks_for_rspec
+        RR::Space.instance.verify_doubles
+      end
+
+      def teardown_mocks_for_rspec
+        RR::Space.instance.reset
+      end
+
     end
   end
 end
