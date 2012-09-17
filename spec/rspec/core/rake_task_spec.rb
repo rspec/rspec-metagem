rspec/rspec-metagem

commit 541260200da8d07ee22181ca3e300eea9f8c9d0a

Break the rake task constructor up a bit
Signed-off-by: Sam Phippen <samphippen@googlemail.com>

lib/rspec/core/rake_task.rb
@@ -110,33 +110,49 @@ module RSpec
       end
 
       def initialize(*args)
+        #configure the rake task
+        self.setup_config args
+        yield self if block_given?
+        self.set_defaults
+
+        desc "Run RSpec code examples" unless ::Rake.application.last_comment
+
+        task name do
+          RakeFileUtils.send(:verbose, verbose) do
+            self.run_task verbose
+          end
+        end
+      end
+
+      def setup_config(*args)
         @name = args.shift || :spec
         @pattern, @rcov_path, @rcov_opts, @ruby_opts, @rspec_opts = nil, nil, nil, nil, nil
         @warning, @rcov = false, false
         @verbose, @fail_on_error = true, true
+      end
 
-        yield self if block_given?
-
+      def set_defaults
         @rcov_path  ||= 'rcov'
         @rspec_path ||= 'rspec'
         @pattern    ||= './spec{,/*/**}/*_spec.rb'
+      end
 
-        desc("Run RSpec code examples") unless ::Rake.application.last_comment
+      def has_files?
+        empty = files_to_run.empty?
+        puts "No examples matching #{pattern} could be found" if empty
+        not empty
+      end
 
-        task name do
-          RakeFileUtils.send(:verbose, verbose) do
-            if files_to_run.empty?
-              puts "No examples matching #{pattern} could be found"
-            else
-              begin
-                puts spec_command if verbose
-                success = system(spec_command)
-              rescue
-                puts failure_message if failure_message
-              end
-              raise("#{spec_command} failed") if fail_on_error unless success
-            end
+      def run_task(verbose)
+        files = self.has_files?
+        if files
+          begin
+            puts spec_command if verbose
+            success = system(spec_command)
+          rescue
+            puts failure_message if failure_message
           end
+          raise("#{spec_command} failed") if fail_on_error unless success
         end
       end
 

commit 2a85bc75beb5a03df4b29f81bb8e2680c35af3a8

Fixes regression introduced by earlier refactoring in 541260200da8d07ee22181ca3e300eea9f8c9d0a
* args was being double wrapped in an Array after being splatted twice

lib/rspec/core/rake_task.rb
@@ -110,8 +110,7 @@ module RSpec
       end
 
       def initialize(*args)
-        #configure the rake task
-        setup_ivars args
+        setup_ivars(args)
         yield self if block_given?
 
         desc "Run RSpec code examples" unless ::Rake.application.last_comment
@@ -123,7 +122,7 @@ module RSpec
         end
       end
 
-      def setup_ivars(*args)
+      def setup_ivars(args)
         @name = args.shift || :spec
         @rcov_opts, @ruby_opts, @rspec_opts = nil, nil, nil
         @warning, @rcov = false, false

spec/rspec/core/rake_task_spec.rb
@@ -19,6 +19,14 @@ module RSpec::Core
       task.__send__(:spec_command)
     end
 
+    context "with a name passed to the constructor" do
+      let(:task) { RakeTask.new(:task_name) }
+
+      it "correctly sets the name" do
+        task.name.should == :task_name
+      end
+    end
+
     context "default" do
       it "renders rspec" do
         spec_command.should =~ /^#{ruby} -S rspec/