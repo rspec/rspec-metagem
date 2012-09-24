518a0f7ff353b1fd5a2a557bd93ab28c7645a909
Sam Phippen <samphippen@googlemail.com>
2012-09-24 13:34:10 +0100

    Warn if the "message" param passed to a should is not a string

--- lib/rspec/expectations/handler.rb
+++ lib/rspec/expectations/handler.rb
@@ -1,18 +1,31 @@
 module RSpec
   module Expectations
-    class PositiveExpectationHandler        
+
+    class ExpectationHandler
+      def self.message_must_be_string
+        "The value passed as the message for the expectation was not a string"
+      end
+
+      def self.check_message(message=nil)
+        ::Kernel.warn message_must_be_string unless message == nil or String === message
+      end
+    end
+
+    class PositiveExpectationHandler < ExpectationHandler
+
       def self.handle_matcher(actual, matcher, message=nil, &block)
+        check_message(message)
         ::RSpec::Matchers.last_should = :should
         ::RSpec::Matchers.last_matcher = matcher
         return ::RSpec::Matchers::BuiltIn::PositiveOperatorMatcher.new(actual) if matcher.nil?
 
         match = matcher.matches?(actual, &block)
         return match if match
-        
+
         message ||= matcher.respond_to?(:failure_message_for_should) ?
                     matcher.failure_message_for_should :
                     matcher.failure_message
-        
+
         if matcher.respond_to?(:diffable?) && matcher.diffable?
           ::RSpec::Expectations.fail_with message, matcher.expected, matcher.actual
         else
@@ -21,17 +34,18 @@ module RSpec
       end
     end
 
-    class NegativeExpectationHandler
+    class NegativeExpectationHandler < ExpectationHandler
       def self.handle_matcher(actual, matcher, message=nil, &block)
+        check_message(message)
         ::RSpec::Matchers.last_should = :should_not
         ::RSpec::Matchers.last_matcher = matcher
         return ::RSpec::Matchers::BuiltIn::NegativeOperatorMatcher.new(actual) if matcher.nil?
-        
+
         match = matcher.respond_to?(:does_not_match?) ?
                 !matcher.does_not_match?(actual, &block) :
                 matcher.matches?(actual, &block)
         return match unless match
-        
+
         message ||= matcher.respond_to?(:failure_message_for_should_not) ?
                     matcher.failure_message_for_should_not :
                     matcher.negative_failure_message
