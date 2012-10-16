280ee60d4c85e8a92fbb0ce22f0414436c8aafd0
Andy Lindeman <alindeman@gmail.com>
2012-10-15 23:53:16 -0400

    Adds generated fixtures for JRuby 1.9
    * Also sets the source encoding for the specs to UTF-8. For some reason,
      Nokogiri did not correctly parse the actual output if they were read
      into ASCII strings. Setting the source encoding seems to solve this,
      even though I thought source encoding only affected the source file
      itself, not encodings of I/O.

--- spec/rspec/core/formatters/html_formatter_spec.rb
+++ spec/rspec/core/formatters/html_formatter_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 require 'rspec/core/formatters/html_formatter'
 require 'nokogiri'
--- spec/rspec/core/formatters/text_mate_formatter_spec.rb
+++ spec/rspec/core/formatters/text_mate_formatter_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 require 'rspec/core/formatters/text_mate_formatter'
 require 'nokogiri'
