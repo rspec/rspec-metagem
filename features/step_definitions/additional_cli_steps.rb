b15a03b91646662b31ffea8d9555e5d90b6ba9e7
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-11-04 12:04:35 -0500

    add feature to document failure_exit_code option

--- features/step_definitions/additional_cli_steps.rb
+++ features/step_definitions/additional_cli_steps.rb
@@ -30,3 +30,7 @@ Then /^the backtrace\-normalized output should contain:$/ do |partial_output|
 
   normalized_output.should =~ regexp(partial_output)
 end
+
+Then /^the exit code should be (\d+)$/ do |code|
+  $?.exitstatus.should == code.to_i
+end
