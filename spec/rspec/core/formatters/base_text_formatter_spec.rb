f256f1bdb3bc516ba622a8f34e810e70f31c0bb2
Myron Marston <myron.marston@gmail.com>
2012-12-24 08:59:24 -0800

    Re-instate the sleep -- the build failed on JRuby.
    Locally, it passed on JRuby for me but failed on travis.
    It failed due to reporting the time taken as 0% instead of 100%.
    
    So, we need to ensure some time passes, but it doesn't need to be much.

--- spec/rspec/core/formatters/base_text_formatter_spec.rb
+++ spec/rspec/core/formatters/base_text_formatter_spec.rb
@@ -344,7 +344,9 @@ describe RSpec::Core::Formatters::BaseTextFormatter do
   describe "#dump_profile" do
     before do
       group = RSpec::Core::ExampleGroup.describe("group") do
-        example("example") { }
+        # Use a sleep so there is some measurable time, to ensure
+        # the reported percent is 100%, not 0%.
+        example("example") { sleep 0.001 }
       end
       group.run(double('reporter').as_null_object)
 
