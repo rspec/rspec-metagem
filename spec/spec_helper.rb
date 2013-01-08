06a84fe89c6db88ba4f71f15119ccee1c9b9fbde
Myron Marston <myron.marston@gmail.com>
2013-01-07 23:28:56 -0800

    No need for this load path management.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,4 @@
-def add_to_load_path(path, prepend=false)
-  path = File.expand_path("../../#{path}/lib", __FILE__)
-  if prepend
-    $LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path)
-  else
-    $LOAD_PATH << path unless $LOAD_PATH.include?(path)
-  end
-end
-
 require 'test/unit'
-
-add_to_load_path("rspec-expectations", :prepend)
-add_to_load_path("rspec-core")
-add_to_load_path("rspec-mocks")
-
-require 'rspec/expectations'
-require 'rspec/core'
-require 'rspec/mocks'
 
 Dir['./spec/support/**/*'].each {|f| require f}
 
