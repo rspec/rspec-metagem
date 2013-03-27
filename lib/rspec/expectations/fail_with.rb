samphippen/rspec-expectations

Change the differ for arrays of strings with newlines in them.
Signed-off-by: Sam Phippen <samphippen@googlemail.com>

lib/rspec/expectations/fail_with.rb
@@ -52,7 +52,17 @@ module RSpec
 
       def coerce_to_string(string_or_array)
         return string_or_array unless Array === string_or_array
-        string_or_array.join(',')
+        diffably_stringify(string_or_array).join("\n")
+      end
+
+      def diffably_stringify(array)
+        array.map do |entry|
+          if Array === entry
+            entry.to_s
+          else
+            entry.to_s.gsub("\n", "\\n")
+          end
+        end
       end
 
       if String.method_defined?(:encoding)

rspec/rspec-metagem

lib/rspec/expectations/fail_with.rb
@@ -52,7 +52,17 @@ module RSpec
 
       def coerce_to_string(string_or_array)
         return string_or_array unless Array === string_or_array
-        string_or_array.join(',')
+        diffably_stringify(string_or_array).join("\n")
+      end
+
+      def diffably_stringify(array)
+        array.map do |entry|
+          if Array === entry
+            entry.inspect
+          else
+            entry.to_s.gsub("\n", "\\n")
+          end
+        end
       end
 
       if String.method_defined?(:encoding)