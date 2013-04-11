f2b15a3976ef74d01921771c3087fad1146f94cd
Jon Rowe <hello@jonrowe.co.uk>
2013-04-11 15:12:18 +1000

    force diff-lcs 122 and remove workaround

--- lib/rspec/expectations/differ.rb
+++ lib/rspec/expectations/differ.rb
@@ -28,13 +28,7 @@ module RSpec
             # diff includes lines of context. Otherwise, we might print
             # redundant lines.
             if (context_lines > 0) and hunk.overlaps?(oldhunk)
-              if hunk.respond_to?(:merge)
-                # diff-lcs 1.2.x
-                hunk.merge(oldhunk)
-              else
-                # diff-lcs 1.1.3
-                hunk.unshift(oldhunk)
-              end
+              hunk.merge(oldhunk)
             else
               output << matching_encoding(oldhunk.diff(format).to_s, output)
             end
