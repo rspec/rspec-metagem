77a3ec724b96d9cbb7c2efc0ac25eb575a8ced13
Parker Selbert <parker@sorentwo.com>
2013-04-18 06:38:29 -0500

    Comment out and document the generated config
    All of the settings within the config file are slated to become the
    default for 3.0. In the meantime comment them out of the generated
    configuration and provide explanitory comments, hopefully to reduce any
    confusion for people who are starting new projects.

--- lib/rspec/core/project_initializer.rb
+++ lib/rspec/core/project_initializer.rb
@@ -31,9 +31,9 @@ CONTENT
 
       def create_spec_helper_file
         if File.exist?('spec/spec_helper.rb')
-          report_exists("spec/spec_helper.rb")
+          report_exists('spec/spec_helper.rb')
         else
-          report_creating("spec/spec_helper.rb")
+          report_creating('spec/spec_helper.rb')
           FileUtils.mkdir_p('spec')
           File.open('spec/spec_helper.rb','w') do |f|
             f.write <<-CONTENT
@@ -44,14 +44,21 @@ CONTENT
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  # Limit the spec run to only specs with the focus metadata. If no specs have
+  # the filtering metadata and `run_all_when_everything_filtered = true` then
+  # all specs will run.
+  #config.filter_run :focus
+
+  # Run all specs when none match the provided filter. This works well in
+  # conjunction with `config.filter_run :focus`, as it will run the entire
+  # suite when no specs have `:filter` metadata.
+  #config.run_all_when_everything_filtered = true
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = 'random'
+  #config.order = 'random'
 end
 CONTENT
           end
