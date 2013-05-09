3d76bea2e3494dcd360fd7efe547dc2f16ec44ba
tomykaira <tomykaira@gmail.com>
2013-05-09 23:00:43 +0900

    Warn when raise_error receives non-proc object
    When a non-proc object is given, `given_proc.call` fails, then `raise_error` or
    `raise_error(NoMethodError)` passes.
    
    This behaviour is dangerous and there is actually no error, so it should warn
    and fail.

--- lib/rspec/matchers/built_in/raise_error.rb
+++ lib/rspec/matchers/built_in/raise_error.rb
@@ -24,6 +24,10 @@ module RSpec
           @with_expected_message = false
           @eval_block = false
           @eval_block_passed = false
+          unless given_proc.respond_to?(:call)
+            ::Kernel.warn "`raise_error` is called with non-proc object"
+            return false
+          end
           begin
             given_proc.call
           rescue Exception => @actual_error
