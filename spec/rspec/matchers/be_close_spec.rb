e455a84e2ffd7c4d4b05c679eee4c347efbba27c
David Chelimsky <dchelimsky@gmail.com>
2013-05-18 09:52:08 -0500

    fix be_close spec (broken by changes to rspec-core)

--- spec/rspec/matchers/be_close_spec.rb
+++ spec/rspec/matchers/be_close_spec.rb
@@ -4,17 +4,17 @@ module RSpec
   module Matchers
     describe "expect(actual).to be_close(expected, delta)" do
       before(:each) do
-        RSpec.stub(:warn)
+        allow(RSpec).to receive(:deprecate)
+      end
+
+      it "is deprecated" do
+        expect(RSpec).to receive(:deprecate).with(/be_close.*/,/be_within.*/)
+        be_close(3.0, 0.5)
       end
 
       it "delegates to be_within(delta).of(expected)" do
         should_receive(:be_within).with(0.5).and_return( be_within_matcher = double )
         be_within_matcher.should_receive(:of).with(3.0)
-        be_close(3.0, 0.5)
-      end
-
-      it "prints a deprecation warning" do
-        RSpec.should_receive(:warn).with(/use be_within.*instead/)
         be_close(3.0, 0.5)
       end
     end
