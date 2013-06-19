xaviershay/rspec-core

yield current running example to it/example and before/after hooks

lib/rspec/core/example.rb
@@ -110,7 +110,7 @@ module RSpec
             with_around_each_hooks do
               begin
                 run_before_each
-                @example_group_instance.instance_eval(&@example_block)
+                @example_group_instance.instance_eval_with_args(self, &@example_block)
               rescue Pending::PendingDeclaredInExample => e
                 @pending_declared_in_example = e.message
               rescue Exception => e
@@ -238,7 +238,7 @@ An error occurred #{context}
 
       # @private
       def instance_eval_with_rescue(context = nil, &block)
-        @example_group_instance.instance_eval_with_rescue(context, &block)
+        @example_group_instance.instance_eval_with_rescue(self, context, &block)
       end
 
       # @private

rspec/rspec-metagem

lib/rspec/core/example.rb
@@ -111,7 +111,7 @@ module RSpec
             with_around_each_hooks do
               begin
                 run_before_each
-                @example_group_instance.instance_eval(&@example_block)
+                @example_group_instance.instance_exec(self, &@example_block)
               rescue Pending::PendingDeclaredInExample => e
                 @pending_declared_in_example = e.message
               rescue Exception => e
@@ -239,7 +239,7 @@ An error occurred #{context}
 
       # @private
       def instance_eval_with_rescue(context = nil, &block)
-        @example_group_instance.instance_eval_with_rescue(context, &block)
+        @example_group_instance.instance_eval_with_rescue(self, context, &block)
       end
 
       # @private