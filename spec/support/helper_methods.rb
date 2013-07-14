395ada2b8873f3fdc0eb05257f62c8aaff356192
Myron Marston <myron.marston@gmail.com>
2013-07-13 22:07:20 -0700

    Add helper methods to assist with spec'ing deprecations.
    The most consistently tricky part to get right is the call site,
    so that's what this helper method helps with.

--- spec/support/helper_methods.rb
+++ spec/support/helper_methods.rb
@@ -23,4 +23,14 @@ module RSpecHelpers
     end
   end
 
+  def expect_deprecation_with_call_site(file, line)
+    expect(RSpec.configuration.reporter).to receive(:deprecation) do |options|
+      expect(options[:call_site]).to include([file, line].join(':'))
+    end
+  end
+
+  def allow_deprecation
+    allow(RSpec.configuration.reporter).to receive(:deprecation)
+  end
+
 end
