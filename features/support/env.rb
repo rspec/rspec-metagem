be25ba724c45734813ebfc163624a3b07aac84cd
Federico Ravasio <ravasio.federico@gmail.com>
2013-07-14 06:20:25 -0700

    Disable Rubinius' JIT when running aruba tests.

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,3 +14,8 @@ Aruba.configure do |config|
   end
 end if RUBY_PLATFORM == 'java'
 
+Aruba.configure do |config|
+  config.before_cmd do |cmd|
+    set_env('RBXOPT', "-Xint=true #{ENV['RBXOPT']}") # disable JIT since these processes are so short lived
+  end
+end if defined?(Rubinius)
