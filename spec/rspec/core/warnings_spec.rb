ffc9fd2300ddae1da7f3e9af6b925f389875507c
Jon Rowe <hello@jonrowe.co.uk>
2013-07-28 20:52:50 +1000

    warning helper with deprecations refactored

--- spec/rspec/core/warnings_spec.rb
+++ spec/rspec/core/warnings_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe RSpec::Core::Deprecation do
+describe "RSpec deprecations and warnings" do
   describe "#deprecate" do
     it "passes the hash to the reporter" do
       expect(RSpec.configuration.reporter).to receive(:deprecation).with(hash_including :deprecated => "deprecated_method", :replacement => "replacement")
@@ -23,5 +23,29 @@ describe RSpec::Core::Deprecation do
       expect(RSpec.configuration.reporter).to receive(:deprecation).with(hash_including :message => "this is the message")
       RSpec.warn_deprecation("this is the message")
     end
+  end
+
+  shared_examples_for "warning helper" do |helper|
+    it 'warns with the message text' do
+      expect(::Kernel).to receive(:warn).with /Message/
+      RSpec.send(helper, 'Message')
+    end
+
+    it 'sets the calling line' do
+      expect(::Kernel).to receive(:warn).with /#{__FILE__}:#{__LINE__+1}/
+      RSpec.send(helper, 'Message')
+    end
+  end
+
+  describe "#warning" do
+    it 'prepends WARNING:' do
+      expect(::Kernel).to receive(:warn).with /WARNING: Message\./
+      RSpec.warning 'Message'
+    end
+    it_should_behave_like 'warning helper', :warning
+  end
+
+  describe "#warn_with message, options" do
+    it_should_behave_like 'warning helper', :warn_with
   end
 end
