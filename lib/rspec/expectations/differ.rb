56d9fe919f08377e82b383f581d30fdcb6535c66
Jon Rowe <hello@jonrowe.co.uk>
2013-08-02 22:29:56 +1000

    comment explaining PP

--- lib/rspec/expectations/differ.rb
+++ lib/rspec/expectations/differ.rb
@@ -111,7 +111,10 @@ module RSpec
             pp_key   = PP.singleline_pp(key, "")
             pp_value = PP.singleline_pp(object[key], "")
 
-            #on 1.9.3 PP seems to minimise to US-ASCII, ensure we're matching source encoding
+            # on 1.9.3 PP seems to minimise to US-ASCII, ensure we're matching source encoding
+            #
+            # note, PP is used to ensure the ordering of the internal values of key/value e.g.
+            # <# a: b: c:> not <# c: a: b:>
             matching_encoding("#{pp_key} => #{pp_value}", key)
           end.join(",\n")
         when String
