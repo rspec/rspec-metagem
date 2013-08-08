1623cd0dd8f1d0bfc8f6756a94c7b39cdbb3503e
Jon Rowe <hello@jonrowe.co.uk>
2013-08-08 12:55:28 +1000

    assert both messages are received

--- spec/rspec/core/example_group_spec.rb
+++ spec/rspec/core/example_group_spec.rb
@@ -647,8 +647,8 @@ module RSpec::Core
 
         let(:group) do
           ExampleGroup.describe do
-            after(:all) { hooks_run << :one; raise "error in after all" }
-            after(:all) { hooks_run << :two; raise "second error in after all" }
+            after(:all) { hooks_run << :one; raise "An error in an after(:all) hook" }
+            after(:all) { hooks_run << :two; raise "A different hook raising an error" }
             it("equality") { expect(1).to eq(1) }
           end
         end
@@ -661,8 +661,9 @@ module RSpec::Core
           expect(example.metadata[:execution_result][:status]).to eq("passed")
         end
 
-        it "rescues the error and prints it out" do
-          RSpec.configuration.reporter.should_receive(:message).with(/error in after all/)
+        it "rescues any error(s) and prints them out" do
+          RSpec.configuration.reporter.should_receive(:message).with(/An error in an after\(:all\) hook/)
+          RSpec.configuration.reporter.should_receive(:message).with(/A different hook raising an error/)
           group.run
         end
 
