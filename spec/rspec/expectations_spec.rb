a55844a4c53b2d0bc97a747c460da56ac604c173
Myron Marston <myron.marston@gmail.com>
2013-08-23 07:16:03 -0700

    Fix load path search.
    It worked fine before with a local bundler path, but not
    a :git path -- the :git path includes the SHA in the
    directory.

--- spec/rspec/expectations_spec.rb
+++ spec/rspec/expectations_spec.rb
@@ -1,14 +1,15 @@
 module RSpec
   describe Expectations do
     def file_contents_for(lib, filename)
-      path = $LOAD_PATH.find { |p| p.include?("/#{lib}/lib") }
+      # http://rubular.com/r/HYpUMftlG2
+      path = $LOAD_PATH.find { |p| p.match(/\/rspec-#{lib}(-[a-f0-9]+)?\/lib/) }
       file = File.join(path, filename)
       File.read(file)
     end
 
     it 'has an up-to-date rspec/caller_filter file' do
-      expectations = file_contents_for("rspec-expectations", "rspec/caller_filter.rb")
-      core         = file_contents_for("rspec-core",         "rspec/caller_filter.rb")
+      expectations = file_contents_for("expectations", "rspec/caller_filter.rb")
+      core         = file_contents_for("core",         "rspec/caller_filter.rb")
 
       expect(expectations).to eq(core)
     end
