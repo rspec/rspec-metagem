404ae357e75008fed1e668bc0e6415a6b874460d
Myron Marston <myron.marston@gmail.com>
2013-08-25 23:28:27 -0700

    Remove deprecated config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,6 @@ end
 Dir['./spec/support/**/*'].each {|f| require f}
 
 RSpec::configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.color_enabled = true
   config.filter_run :focused
   config.run_all_when_everything_filtered = true
