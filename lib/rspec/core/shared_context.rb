70049d1f2c40e4e898dcff03edc32b623cda15e3
Jon Rowe <hello@jonrowe.co.uk>
2013-09-25 08:23:44 +1000

    remove a stray string eval @xaviershay reported in #862

--- lib/rspec/core/shared_context.rb
+++ lib/rspec/core/shared_context.rb
@@ -38,11 +38,9 @@ module RSpec
       # @api private
       def self.record(methods)
         methods.each do |meth|
-          class_eval <<-EOS, __FILE__, __LINE__ + 1
-            def #{meth}(*args, &block)
-              __shared_context_recordings << Recording.new(:#{meth}, args, block)
-            end
-          EOS
+          define_method(meth) do |*args, &block|
+            __shared_context_recordings << Recording.new(meth, args, block)
+          end
         end
       end
 
