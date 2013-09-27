a9d40ab6e025718d4b1edfecc5df419ff619ec0d
Jay Hayes <ur@iamvery.com>
2013-09-27 10:04:31 -0500

    Add spec for formatters/helpers mathn integration

--- spec/rspec/core/formatters/helpers_spec.rb
+++ spec/rspec/core/formatters/helpers_spec.rb
@@ -46,6 +46,16 @@ describe RSpec::Core::Formatters::Helpers do
         expect(helper.format_duration(1)).to eq("1 second")
       end
     end
+
+    context 'with mathn loaded' do
+      include MathnIntegrationSupport
+
+      it "returns 'x minutes xx.x seconds' formatted string" do
+        with_mathn_loaded do
+          expect(helper.format_duration(133.7)).to eq("2 minutes 13.7 seconds")
+        end
+      end
+    end
   end
 
   describe "format seconds" do
