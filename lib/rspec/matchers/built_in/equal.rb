9dbb57847417a520075eae26fff2e7a7b422833c
Sam Phippen <samphippen@googlemail.com>
2013-11-07 12:32:04 -0500

    Use a simpler message for identity on singleton expectations

--- lib/rspec/matchers/built_in/equal.rb
+++ lib/rspec/matchers/built_in/equal.rb
@@ -7,17 +7,18 @@ module RSpec
         end
 
         def failure_message_for_should
-          return <<-MESSAGE
-
-expected #{inspect_object(expected)}
-     got #{inspect_object(actual)}
-
-Compared using equal?, which compares object identity,
-but expected and actual are not the same object. Use
-`#{eq_expression}` if you don't care about
-object identity in this example.
-
-MESSAGE
+          @diffable = false
+          case expected
+          when FalseClass
+            builtin_singleton_message("false")
+          when TrueClass
+            builtin_singleton_message("true")
+          when NilClass
+            builtin_singleton_message("nil")
+          else
+            @diffable = true
+            arbitrary_object_message
+          end
         end
 
         def failure_message_for_should_not
@@ -31,9 +32,30 @@ Compared using equal?, which compares object identity.
 MESSAGE
         end
 
-        def diffable?; true; end
+        def diffable?
+          @diffable
+        end
 
         private
+
+        def builtin_singleton_message(type)
+          "\nexpected #{type}\n     got #{inspect_object(actual)}\n"
+        end
+
+        def arbitrary_object_message
+          return <<-MESSAGE
+
+expected #{inspect_object(expected)}
+     got #{inspect_object(actual)}
+
+Compared using equal?, which compares object identity,
+but expected and actual are not the same object. Use
+`#{eq_expression}` if you don't care about
+object identity in this example.
+
+MESSAGE
+        end
+
 
         def inspect_object(o)
           "#<#{o.class}:#{o.object_id}> => #{o.inspect}"
