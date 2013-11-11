c7f5a3ef8400e02ffb70628457419517bea83b27
Myron Marston <myron.marston@gmail.com>
2013-11-10 20:40:48 -0800

    Move method_cache_busters script to script directory.

--- script/method_cache_busters.sh
+++ script/method_cache_busters.sh
@@ -4,7 +4,7 @@
 # This list is from https://charlie.bz/blog/things-that-clear-rubys-method-cache
 
 IGNORE_FILE=/tmp/cache_busters_ignore
-cat ignores | ruby -ne 'puts $_.split(/\s+###/)[0]' > $IGNORE_FILE
+cat script/ignores | ruby -ne 'puts $_.split(/\s+###/)[0]' > $IGNORE_FILE
 
 egrep 'def [a-z]*\..*' -R lib | grep -v "def self" | grep -v -f $IGNORE_FILE
 grep undef -R lib | grep -v -f $IGNORE_FILE
