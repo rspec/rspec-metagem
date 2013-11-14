dc61e818915fca8d5ace25b2a69aed7584f43545
Ben Hoskings <ben@hoskings.net>
2013-11-14 12:02:35 +1100

    Update some stubs in the runner specs (stale since e76da65).

--- spec/rspec/core/runner_spec.rb
+++ spec/rspec/core/runner_spec.rb
@@ -18,18 +18,18 @@ module RSpec::Core
 
     describe 'at_exit' do
       it 'sets an at_exit hook if none is already set' do
+        RSpec::Core::Runner.stub(:autorun_disabled?).and_return(false)
         RSpec::Core::Runner.stub(:installed_at_exit?).and_return(false)
         RSpec::Core::Runner.stub(:running_in_drb?).and_return(false)
-        RSpec::Core::Runner.stub(:at_exit_hook_disabled?).and_return(false)
         RSpec::Core::Runner.stub(:invoke)
         RSpec::Core::Runner.should_receive(:at_exit)
         RSpec::Core::Runner.autorun
       end
 
       it 'does not set the at_exit hook if it is already set' do
+        RSpec::Core::Runner.stub(:autorun_disabled?).and_return(false)
         RSpec::Core::Runner.stub(:installed_at_exit?).and_return(true)
         RSpec::Core::Runner.stub(:running_in_drb?).and_return(false)
-        RSpec::Core::Runner.stub(:at_exit_hook_disabled?).and_return(false)
         RSpec::Core::Runner.should_receive(:at_exit).never
         RSpec::Core::Runner.autorun
       end
