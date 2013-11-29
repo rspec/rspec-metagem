1e8b1aad081d1bc37c5d237abb591596bbb16e97
Myron Marston <myron.marston@gmail.com>
2013-11-29 13:05:40 -0800

    Remove dead code.

--- spec/rspec/expectations_spec.rb
+++ spec/rspec/expectations_spec.rb
@@ -1,12 +1,5 @@
 module RSpec
   describe Expectations do
-    def file_contents_for(lib, filename)
-      # http://rubular.com/r/HYpUMftlG2
-      path = $LOAD_PATH.find { |p| p.match(/\/rspec-#{lib}(-[a-f0-9]+)?\/lib/) }
-      file = File.join(path, filename)
-      File.read(file)
-    end
-
     describe '.method_handle_for(object, method_name)' do
 
       class UntamperedClass
