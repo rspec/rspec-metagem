ec07bb19254bc26b3460549aaf848574ce122b8d
Myron Marston <myron.marston@gmail.com>
2013-12-17 20:14:54 -0800

    Refactor `include` matcher a bit: pass around fewer args.
    We can leverage the instance state of the matcher
    object.

--- lib/rspec/matchers/built_in/include.rb
+++ lib/rspec/matchers/built_in/include.rb
@@ -8,12 +8,12 @@ module RSpec
 
         def matches?(actual)
           @actual = actual
-          perform_match(:all?, :all?, @actual, @expected)
+          perform_match(:all?, :all?)
         end
 
         def does_not_match?(actual)
           @actual = actual
-          perform_match(:none?, :any?, @actual, @expected)
+          perform_match(:none?, :any?)
         end
 
         def description
@@ -23,37 +23,37 @@ module RSpec
         def diffable?
           # Matchers do not diff well, since diff uses their inspect
           # output, which includes their instance variables and such.
-          @expected.none? { |e| RSpec::Matchers.is_a_matcher?(e) }
+          expected.none? { |e| RSpec::Matchers.is_a_matcher?(e) }
         end
 
         private
 
-        def perform_match(predicate, hash_predicate, actuals, expecteds)
-          expecteds.__send__(predicate) do |expected|
-            if comparing_hash_values?(actuals, expected)
-              expected.__send__(hash_predicate) { |k,v|
-                actuals.has_key?(k) && actuals[k] == v
+        def perform_match(predicate, hash_predicate)
+          expected.__send__(predicate) do |expected_item|
+            if comparing_hash_values?(expected_item)
+              expected_item.__send__(hash_predicate) { |k,v|
+                actual.has_key?(k) && actual[k] == v
               }
-            elsif comparing_hash_keys?(actuals, expected)
-              actuals.has_key?(expected)
-            elsif comparing_with_matcher?(actual, expected)
-              actual.any? { |value| expected.matches?(value) }
+            elsif comparing_hash_keys?(expected_item)
+              actual.has_key?(expected_item)
+            elsif comparing_with_matcher?(expected_item)
+              actual.any? { |value| expected_item.matches?(value) }
             else
-              actuals.include?(expected)
+              actual.include?(expected_item)
             end
           end
         end
 
-        def comparing_hash_keys?(actual, expected)
-          actual.is_a?(Hash) && !expected.is_a?(Hash)
+        def comparing_hash_keys?(expected_item)
+          actual.is_a?(Hash) && !expected_item.is_a?(Hash)
         end
 
-        def comparing_hash_values?(actual, expected)
-          actual.is_a?(Hash) && expected.is_a?(Hash)
+        def comparing_hash_values?(expected_item)
+          actual.is_a?(Hash) && expected_item.is_a?(Hash)
         end
 
-        def comparing_with_matcher?(actual, expected)
-          actual.is_a?(Array) && RSpec::Matchers.is_a_matcher?(expected)
+        def comparing_with_matcher?(expected_item)
+          actual.is_a?(Array) && RSpec::Matchers.is_a_matcher?(expected_item)
         end
       end
     end
