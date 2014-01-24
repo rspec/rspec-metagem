97a473c33d0c87c1aedb0c61a60b76f89815c3e6
Jon Rowe <hello@jonrowe.co.uk>
2014-01-24 13:23:38 +1100

    upgrade legacy formatter

--- lib/rspec/core/formatters/legacy_formatter.rb
+++ lib/rspec/core/formatters/legacy_formatter.rb
@@ -34,74 +34,74 @@ module RSpec
         # @api public
         #
         # @param example_count
-        def start(example_count)
-          @formatter.start example_count
+        def start(notification)
+          @formatter.start notification.count
         end
 
         # @api public
         #
         # @param example_group
-        def example_group_started(group)
-          @formatter.example_group_started group
+        def example_group_started(notification)
+          @formatter.example_group_started notification.group
         end
 
         # @api public
         #
         # @param example_group
-        def example_group_finished(group)
-          @formatter.example_group_finished group
+        def example_group_finished(notification)
+          @formatter.example_group_finished notification.group
         end
 
         # @api public
         #
         # @param example
-        def example_started(example)
-          @formatter.example_started example
+        def example_started(notification)
+          @formatter.example_started notification.example
         end
 
         # @api public
         #
         # @param example
-        def example_passed(example)
-          @formatter.example_passed example
+        def example_passed(notification)
+          @formatter.example_passed notification.example
         end
 
         # @api public
         #
         # @param example
-        def example_pending(example)
-          @formatter.example_pending example
+        def example_pending(notification)
+          @formatter.example_pending notification.example
         end
 
         # @api public
         #
         # @param example
-        def example_failed(example)
-          @formatter.example_failed example
+        def example_failed(notification)
+          @formatter.example_failed notification.example
         end
 
         # @api public
         #
         # @param message
-        def message(message)
-          @formatter.message message
+        def message(notification)
+          @formatter.message notification.message
         end
 
         # @api public
         #
-        def stop
+        def stop(notification)
           @formatter.stop
         end
 
         # @api public
         #
-        def start_dump
+        def start_dump(notification)
           @formatter.start_dump
         end
 
         # @api public
         #
-        def dump_failures
+        def dump_failures(notification)
           @formatter.dump_failures
         end
 
@@ -111,32 +111,32 @@ module RSpec
         # @param example_count
         # @param failure_count
         # @param pending_count
-        def dump_summary(duration, example_count, failure_count, pending_count)
-          @formatter.dump_summary duration, example_count, failure_count, pending_count
+        def dump_summary(summary)
+          @formatter.dump_summary summary.duration, summary.examples, summary.failures, summary.pending
         end
 
         # @api public
         #
-        def dump_pending
+        def dump_pending(notification)
           @formatter.dump_pending
         end
 
         # @api public
         #
-        def dump_profile
+        def dump_profile(notification)
           @formatter.dump_profile
         end
 
         # @api public
         #
         # @param seed
-        def seed(number)
-          @formatter.seed number
+        def seed(notification)
+          @formatter.seed notification.seed
         end
 
         # @api public
         #
-        def close
+        def close(notification)
           @formatter.close
         end
 
--- spec/rspec/core/formatters/legacy_formatter_spec.rb
+++ spec/rspec/core/formatters/legacy_formatter_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe RSpec::Core::Formatters::LegacyFormatter do
 
   describe "#start" do
     it "notifies formatter of start" do
-      send_notification :start, 5
+      send_notification :start, double(count: 5)
       expect(output.string).to include "Started 5 examples"
     end
   end
@@ -18,7 +18,7 @@ RSpec.describe RSpec::Core::Formatters::LegacyFormatter do
     let(:group) { class_double "RSpec::Core::ExampleGroup", :description => "Group" }
 
     it "notifies formatter of example_group_started" do
-      send_notification :example_group_started, group
+      send_notification :example_group_started, double(group: group)
       expect(output.string).to include "Started Group"
     end
   end
@@ -27,7 +27,7 @@ RSpec.describe RSpec::Core::Formatters::LegacyFormatter do
     let(:group) { class_double "RSpec::Core::ExampleGroup", :description => "Group" }
 
     it "notifies formatter of example_group_finished" do
-      send_notification :example_group_finished, group
+      send_notification :example_group_finished, double(group: group)
       expect(output.string).to include "Finished Group"
     end
   end
@@ -36,7 +36,7 @@ RSpec.describe RSpec::Core::Formatters::LegacyFormatter do
     let(:example) { instance_double "RSpec::Core::Example", :full_description => "Example" }
 
     it "notifies formatter of example_started" do
-      send_notification :example_started, example
+      send_notification :example_started, double(example: example)
       expect(output.string).to include "Started Example"
     end
   end
@@ -45,7 +45,7 @@ RSpec.describe RSpec::Core::Formatters::LegacyFormatter do
     let(:example) { double "example" }
 
     it "notifies formatter of example_passed" do
-      send_notification :example_passed, example
+      send_notification :example_passed, double(example: example)
       expect(output.string).to include "."
     end
   end
@@ -54,7 +54,7 @@ RSpec.describe RSpec::Core::Formatters::LegacyFormatter do
     let(:example) { double "example" }
 
     it "notifies formatter of example_pending" do
-      send_notification :example_pending, example
+      send_notification :example_pending, double(example: example)
       expect(output.string).to include "P"
     end
   end
@@ -63,35 +63,35 @@ RSpec.describe RSpec::Core::Formatters::LegacyFormatter do
     let(:example) { double "example" }
 
     it "notifies formatter of example_failed" do
-      send_notification :example_failed, example
+      send_notification :example_failed, double(example: example)
       expect(output.string).to include "F"
     end
   end
 
   describe "#message" do
     it "notifies formatter of message" do
-      send_notification :message, "A Message"
+      send_notification :message, double(message: "A Message")
       expect(output.string).to include "A Message"
     end
   end
 
   describe "#stop" do
     it "notifies formatter of stop" do
-      send_notification :stop
+      send_notification :stop, double
       expect(output.string).to include "Stopped"
     end
   end
 
   describe "#start_dump" do
     it "notifies formatter of start_dump" do
-      send_notification :start_dump
+      send_notification :start_dump, double
       expect(output.string).to include "Dumping!"
     end
   end
 
   describe "#dump_failures" do
     it "notifies formatter of dump_failures" do
-      send_notification :dump_failures
+      send_notification :dump_failures, double
       expect(output.string).to include "Failures:"
     end
   end
@@ -99,7 +99,7 @@ RSpec.describe RSpec::Core::Formatters::LegacyFormatter do
   describe "#dump_summary" do
     it "notifies formatter of dump_summary" do
       duration, count, failures, pending = 3.5, 10, 3, 2
-      send_notification :dump_summary, duration, count, failures, pending
+      send_notification :dump_summary, RSpec::Core::Reporter::SummaryNotification.new(duration, count, failures, pending)
       expect(output.string).to match "Finished in 3.5"
       expect(output.string).to match "3/10 failed."
       expect(output.string).to match "2 pending."
@@ -108,21 +108,21 @@ RSpec.describe RSpec::Core::Formatters::LegacyFormatter do
 
   describe "#dump_pending" do
     it "notifies formatter of dump_pending" do
-      send_notification :dump_pending
+      send_notification :dump_pending, double
       expect(output.string).to match "Pending:"
     end
   end
 
   describe "#seed" do
     it "notifies formatter of seed" do
-      send_notification :seed, 17
+      send_notification :seed, double(seed: 17)
       expect(output.string).to match "Randomized with seed 17"
     end
   end
 
   describe "#close" do
     it "notifies formatter of close" do
-      send_notification :close
+      send_notification :close, double
     end
   end
 end
