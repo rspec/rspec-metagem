8a76f465db27f2d3424c3472be876b2d96e133dd
Jon Rowe <hello@jonrowe.co.uk>
2014-02-25 12:01:07 +1100

    Updated travis build scripts (from rspec-dev)

--- script/functions.sh
+++ script/functions.sh
@@ -1,4 +1,4 @@
-# This file was generated on 2014-02-12T08:24:06+11:00 from the rspec-dev repo.
+# This file was generated on 2014-02-25T12:00:45+11:00 from the rspec-dev repo.
 # DO NOT modify it by hand as your changes will get lost the next time it is generated.
 
 # idea taken from: http://blog.headius.com/2010/03/jruby-startup-time-tips.html
