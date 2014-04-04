6f825b77d267474e3472744bf6ebde19413172cc
Myron Marston <myron.marston@gmail.com>
2014-04-03 22:18:14 -0700

    Add copy logic to MatcherDelegator.

--- spec/rspec/matchers/aliased_matcher_spec.rb
+++ spec/rspec/matchers/aliased_matcher_spec.rb
@@ -2,6 +2,8 @@ module RSpec
   module Matchers
     describe AliasedMatcher do
       RSpec::Matchers.define :my_base_matcher do
+        match { |actual| actual == foo }
+
         def foo
           13
         end
@@ -10,6 +12,37 @@ module RSpec
           "my base matcher description"
         end
       end
+
+      shared_examples "making a copy" do |copy_method|
+        context "when making a copy via `#{copy_method}`" do
+          it "uses a copy of the base matcher" do
+            base_matcher = include(3)
+            aliased = AliasedMatcher.new(base_matcher, Proc.new {})
+            copy = aliased.__send__(copy_method)
+
+            expect(copy).not_to equal(aliased)
+            expect(copy.base_matcher).not_to equal(base_matcher)
+            expect(copy.base_matcher).to be_a(RSpec::Matchers::BuiltIn::Include)
+            expect(copy.base_matcher.expected).to eq([3])
+          end
+
+          it "copies custom matchers properly so they can work even though they have singleton behavior" do
+            base_matcher = my_base_matcher
+            aliased = AliasedMatcher.new(base_matcher, Proc.new {})
+            copy = aliased.__send__(copy_method)
+
+            expect(copy).not_to equal(aliased)
+            expect(copy.base_matcher).not_to equal(base_matcher)
+
+            expect(13).to copy
+
+            expect { expect(15).to copy }.to fail_with(/expected 15/)
+          end
+        end
+      end
+
+      include_examples "making a copy", :dup
+      include_examples "making a copy", :clone
 
       it 'can get a method object for delegated methods', :if => (RUBY_VERSION.to_f > 1.8) do
         matcher = my_base_matcher
