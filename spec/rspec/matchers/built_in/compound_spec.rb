a0588ab4cf65995bff34944b7be016b8a1066c07
Adam Farhi <afarhi@ebay.com>
2014-04-22 09:49:32 +0300

    Add Boolean AND, OR operators (&, |)

--- spec/rspec/matchers/built_in/compound_spec.rb
+++ spec/rspec/matchers/built_in/compound_spec.rb
@@ -69,6 +69,12 @@ module RSpec::Matchers::BuiltIn
         let(:matcher) { eq(3).and be <= 3 }
       end
 
+      context 'when using boolean AND `&` alias' do
+        it_behaves_like "an RSpec matcher", :valid_value => 3, :invalid_value => 4, :disallows_negation => true do
+          let(:matcher) { eq(3) & be_a(Integer) }
+        end
+      end
+
       include_examples "making a copy", :and, :dup
       include_examples "making a copy", :and, :clone
 
@@ -191,6 +197,12 @@ module RSpec::Matchers::BuiltIn
     describe "expect(...).to matcher.or(other_matcher)" do
       it_behaves_like "an RSpec matcher", :valid_value => 3, :invalid_value => 5, :disallows_negation => true do
         let(:matcher) { eq(3).or eq(4) }
+      end
+
+      context 'when using boolean OR `|` alias' do
+        it_behaves_like "an RSpec matcher", :valid_value => 3, :invalid_value => 5, :disallows_negation => true do
+          let(:matcher) { eq(3) | eq(4) }
+        end
       end
 
       include_examples "making a copy", :or, :dup
