6b899b22125b766a75a945c514b150f0d260ec59
Jon Rowe <hello@jonrowe.co.uk>
2014-05-06 22:19:19 +1000

    use instance_doubles

--- spec/rspec/core/notifications_spec.rb
+++ spec/rspec/core/notifications_spec.rb
@@ -4,12 +4,16 @@ require 'rspec/core/notifications'
 RSpec.describe "FailedExampleNotification" do
   # ported from `base_formatter_spec` should be refactored by final
   describe "#read_failed_line" do
-    let(:example) { double(:Example, :file_path => __FILE__, :execution_result => double(:exception => exception)) }
+    let(:example) do
+     instance_double(RSpec::Core::Example,
+                     :file_path => __FILE__,
+                     :execution_result => double(:exception => exception))
+    end
     let(:notification) { ::RSpec::Core::Notifications::FailedExampleNotification.new(example) }
 
     context "when backtrace is a heterogeneous language stack trace" do
       let(:exception) do
-        double(:Exception, :backtrace => [
+        instance_double(Exception, :backtrace => [
           "at Object.prototypeMethod (foo:331:18)",
           "at Array.forEach (native)",
           "at a_named_javascript_function (/some/javascript/file.js:39:5)",
@@ -23,7 +27,7 @@ RSpec.describe "FailedExampleNotification" do
     end
 
     context "when backtrace will generate a security error" do
-      let(:exception) { double(:Exception, :backtrace => [ "#{__FILE__}:#{__LINE__}"]) }
+      let(:exception) { instance_double(Exception, :backtrace => [ "#{__FILE__}:#{__LINE__}"]) }
 
       it "is handled gracefully" do
         safely do
@@ -33,7 +37,7 @@ RSpec.describe "FailedExampleNotification" do
     end
 
     context "when ruby reports a bogus line number in the stack trace" do
-      let(:exception) { double(:Exception, :backtrace => [ "#{__FILE__}:10000000"]) }
+      let(:exception) { instance_double(Exception, :backtrace => [ "#{__FILE__}:10000000"]) }
 
       it "reports the filename and that it was unable to find the matching line" do
         expect(notification.send(:read_failed_line)).to include("Unable to find matching line")
@@ -53,11 +57,11 @@ RSpec.describe "FailedExampleNotification" do
         end
       end
 
-      let(:exception) { double(:Exception, :backtrace => [ "#{__FILE__}:#{__LINE__}"]) }
+      let(:exception) { instance_double(Exception, :backtrace => [ "#{__FILE__}:#{__LINE__}"]) }
 
       it "doesn't hang when file exists" do
         expect(notification.send(:read_failed_line).strip).to eql(
-          %Q[let(:exception) { double(:Exception, :backtrace => [ "\#{__FILE__}:\#{__LINE__}"]) }])
+          %Q[let(:exception) { instance_double(Exception, :backtrace => [ "\#{__FILE__}:\#{__LINE__}"]) }])
       end
 
     end
