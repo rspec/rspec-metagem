esnyder/rspec-core

extract backtrace handling

lib/rspec/core/formatters/base_text_formatter.rb
@@ -40,7 +40,9 @@ module RSpec
             failure.colorize_message_lines_with(ConsoleCodes).each do |line|
               output.puts "#{long_padding}#{line}"
             end
-            dump_backtrace(example)
+            failure.colorize_backtrace(ConsoleCodes).each do |line|
+              output.puts "#{long_padding}#{line}"
+            end
           end
         end
 
@@ -192,12 +194,6 @@ module RSpec
 
         def format_caller(caller_info)
           configuration.backtrace_formatter.backtrace_line(caller_info.to_s.split(':in `block').first)
-        end
-
-        def dump_backtrace(example)
-          format_backtrace(example.execution_result.exception.backtrace, example).each do |backtrace_info|
-            output.puts detail_color("#{long_padding}# #{backtrace_info}")
-          end
         end
 
       end

lib/rspec/core/formatters/html_formatter.rb
@@ -72,7 +72,7 @@ module RSpec
           exception_details = if exception
             {
               :message => exception.message,
-              :backtrace => format_backtrace(exception.backtrace, example).join("\n")
+              :backtrace => failure.backtrace.join("\n")
             }
           else
             false

rspec/rspec-metagem

lib/rspec/core/formatters/base_text_formatter.rb
@@ -37,10 +37,12 @@ module RSpec
           failed_example_notifications.each_with_index do |failure, index|
             output.puts
             output.puts "#{short_padding}#{index.next}) #{failure.description}"
-            failure.colorize_message_lines_with(ConsoleCodes).each do |line|
+            failure.colorized_message_lines(ConsoleCodes).each do |line|
               output.puts "#{long_padding}#{line}"
             end
-            dump_backtrace(example)
+            failure.colorized_formatted_backtrace(ConsoleCodes).each do |line|
+              output.puts "#{long_padding}#{line}"
+            end
           end
         end
 
@@ -192,12 +194,6 @@ module RSpec
 
         def format_caller(caller_info)
           configuration.backtrace_formatter.backtrace_line(caller_info.to_s.split(':in `block').first)
-        end
-
-        def dump_backtrace(example)
-          format_backtrace(example.execution_result.exception.backtrace, example).each do |backtrace_info|
-            output.puts detail_color("#{long_padding}# #{backtrace_info}")
-          end
         end
 
       end

lib/rspec/core/formatters/html_formatter.rb
@@ -72,7 +72,7 @@ module RSpec
           exception_details = if exception
             {
               :message => exception.message,
-              :backtrace => format_backtrace(exception.backtrace, example).join("\n")
+              :backtrace => failure.formatted_backtrace.join("\n")
             }
           else
             false