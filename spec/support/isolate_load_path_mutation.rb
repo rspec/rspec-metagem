5effaaba68efa55e29f61043099ee76b9ae5802b
Myron Marston <myron.marston@gmail.com>
2014-08-01 22:34:04 -0700

    Always isolate $LOAD_PATH rather than opting in to it.
    There were many specs that were mutating the load path
    and I didn’t realize it, because they used
    `ConfigurationOptions#configure`, which would use
    `Configuration#lib=`, which would mutate `$LOAD_PATH`.

--- spec/support/isolate_load_path_mutation.rb
+++
@@ -1,5 +0,0 @@
-RSpec.shared_context "isolate load path mutation" do
-  original_load_path = nil
-  before { original_load_path = $LOAD_PATH.dup }
-  after  { $LOAD_PATH.replace(original_load_path) }
-end
