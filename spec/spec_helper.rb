745ed28738ccd78c6d9ada3aec6f3c186ac3ae1a
Myron Marston <myron.marston@gmail.com>
2014-08-26 22:55:53 -0700

    Address ruby warnings from spec files.
    For rspec/rspec-support#106.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,108 +1,89 @@
 require 'rubygems' if RUBY_VERSION.to_f < 1.9
 
-begin
-  require 'spork'
-rescue LoadError
-  module Spork
-    def self.prefork
-      yield
-    end
+require 'rspec/support/spec'
 
-    def self.each_run
-      yield
+module ArubaLoader
+  extend RSpec::Support::WithIsolatedStdErr
+  with_isolated_stderr do
+    require 'aruba/api'
+  end
+end
+
+class << RSpec
+  attr_writer :configuration, :world
+end
+
+if RUBY_PLATFORM == 'java'
+  # Works around https://jira.codehaus.org/browse/JRUBY-5678
+  require 'fileutils'
+  ENV['TMPDIR'] = File.expand_path('../../tmp', __FILE__)
+  FileUtils.mkdir_p(ENV['TMPDIR'])
+end
+
+$rspec_core_without_stderr_monkey_patch = RSpec::Core::Configuration.new
+
+class RSpec::Core::Configuration
+  def self.new(*args, &block)
+    super.tap do |config|
+      # We detect ruby warnings via $stderr,
+      # so direct our deprecations to $stdout instead.
+      config.deprecation_stream = $stdout
     end
   end
 end
 
-Spork.prefork do
-  require 'rspec/support/spec'
+Dir['./spec/support/**/*.rb'].map {|f| require f}
 
-  module ArubaLoader
-    extend RSpec::Support::WithIsolatedStdErr
-    with_isolated_stderr do
-      require 'aruba/api'
+module EnvHelpers
+  def with_env_vars(vars)
+    original = ENV.to_hash
+    vars.each { |k, v| ENV[k] = v }
+
+    begin
+      yield
+    ensure
+      ENV.replace(original)
     end
   end
 
-  class << RSpec
-    attr_writer :configuration, :world
-  end
+  def without_env_vars(*vars)
+    original = ENV.to_hash
+    vars.each { |k| ENV.delete(k) }
 
-  if RUBY_PLATFORM == 'java'
-    # Works around https://jira.codehaus.org/browse/JRUBY-5678
-    require 'fileutils'
-    ENV['TMPDIR'] = File.expand_path('../../tmp', __FILE__)
-    FileUtils.mkdir_p(ENV['TMPDIR'])
-  end
-
-  $rspec_core_without_stderr_monkey_patch = RSpec::Core::Configuration.new
-
-  class RSpec::Core::Configuration
-    def self.new(*args, &block)
-      super.tap do |config|
-        # We detect ruby warnings via $stderr,
-        # so direct our deprecations to $stdout instead.
-        config.deprecation_stream = $stdout
-      end
+    begin
+      yield
+    ensure
+      ENV.replace(original)
     end
-  end
-
-  Dir['./spec/support/**/*.rb'].map {|f| require f}
-
-  module EnvHelpers
-    def with_env_vars(vars)
-      original = ENV.to_hash
-      vars.each { |k, v| ENV[k] = v }
-
-      begin
-        yield
-      ensure
-        ENV.replace(original)
-      end
-    end
-
-    def without_env_vars(*vars)
-      original = ENV.to_hash
-      vars.each { |k| ENV.delete(k) }
-
-      begin
-        yield
-      ensure
-        ENV.replace(original)
-      end
-    end
-  end
-
-  RSpec.configure do |c|
-    # structural
-    c.alias_it_behaves_like_to 'it_has_behavior'
-    c.include(RSpecHelpers)
-    c.include Aruba::Api, :file_path => /spec\/command_line/
-
-    c.expect_with :rspec do |expectations|
-      expectations.syntax = :expect
-    end
-
-    c.mock_with :rspec do |mocks|
-      mocks.syntax = :expect
-    end
-
-    # runtime options
-    c.raise_errors_for_deprecations!
-    c.color = true
-    c.include EnvHelpers
-    c.filter_run_excluding :ruby => lambda {|version|
-      case version.to_s
-      when "!jruby"
-        RUBY_ENGINE == "jruby"
-      when /^> (.*)/
-        !(RUBY_VERSION.to_s > $1)
-      else
-        !(RUBY_VERSION.to_s =~ /^#{version.to_s}/)
-      end
-    }
   end
 end
 
-Spork.each_run do
+RSpec.configure do |c|
+  # structural
+  c.alias_it_behaves_like_to 'it_has_behavior'
+  c.include(RSpecHelpers)
+  c.include Aruba::Api, :file_path => /spec\/command_line/
+
+  c.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  c.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+  end
+
+  # runtime options
+  c.raise_errors_for_deprecations!
+  c.color = true
+  c.include EnvHelpers
+  c.filter_run_excluding :ruby => lambda {|version|
+    case version.to_s
+    when "!jruby"
+      RUBY_ENGINE == "jruby"
+    when /^> (.*)/
+      !(RUBY_VERSION.to_s > $1)
+    else
+      !(RUBY_VERSION.to_s =~ /^#{version.to_s}/)
+    end
+  }
 end
