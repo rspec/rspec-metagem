4cd24147ff8be116f0df4110cd1fb8cdd2eda742
Myron Marston <myron.marston@gmail.com>
2014-08-26 22:45:26 -0700

    Extract sandboxing logic into a separate file.
    The formatting specs rely on a particular expected
    backtrace, which depends on which lines in particular
    bits of the sandboxing code are on. Before, changes
    to `spec_helper` would require updating the formatting
    support expected backtraces. This change will isolate it.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,51 +49,6 @@ Spork.prefork do
 
   Dir['./spec/support/**/*.rb'].map {|f| require f}
 
-  class NullObject
-    private
-    def method_missing(method, *args, &block)
-      # ignore
-    end
-  end
-
-  module Sandboxing
-    def self.sandboxed(&block)
-      @orig_config = RSpec.configuration
-      @orig_world  = RSpec.world
-      @orig_example = RSpec.current_example
-      new_config = RSpec::Core::Configuration.new
-      new_config.expose_dsl_globally = false
-      new_config.expecting_with_rspec = true
-      new_world  = RSpec::Core::World.new(new_config)
-      RSpec.configuration = new_config
-      RSpec.world = new_world
-      object = Object.new
-      object.extend(RSpec::Core::SharedExampleGroup)
-
-      (class << RSpec::Core::ExampleGroup; self; end).class_exec do
-        alias_method :orig_run, :run
-        def run(reporter=nil)
-          RSpec.current_example = nil
-          orig_run(reporter || NullObject.new)
-        end
-      end
-
-      RSpec::Mocks.with_temporary_scope do
-        object.instance_exec(&block)
-      end
-    ensure
-      (class << RSpec::Core::ExampleGroup; self; end).class_exec do
-        remove_method :run
-        alias_method :run, :orig_run
-        remove_method :orig_run
-      end
-
-      RSpec.configuration = @orig_config
-      RSpec.world = @orig_world
-      RSpec.current_example = @orig_example
-    end
-  end
-
   module EnvHelpers
     def with_env_vars(vars)
       original = ENV.to_hash
@@ -121,13 +76,6 @@ Spork.prefork do
   RSpec.configure do |c|
     # structural
     c.alias_it_behaves_like_to 'it_has_behavior'
-    c.around {|example| Sandboxing.sandboxed { example.run }}
-    c.around do |ex|
-      orig_load_path = $LOAD_PATH.dup
-      ex.run
-      $LOAD_PATH.replace(orig_load_path)
-    end
-
     c.include(RSpecHelpers)
     c.include Aruba::Api, :file_path => /spec\/command_line/
 
