e8e4541394a411a6c2575253be91095e908d070e
Jon Rowe <hello@jonrowe.co.uk>
2014-10-09 22:06:35 +1100

    demonstate issue with wonky constants on 1.9.2

---
+++ spec/rspec/core/example_group_constants_spec.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+require 'spec_helper'
+
+RSpec.describe "::RSpec::Core::ExampleGroup" do
+  context "does not cause problems when users reference a top level constant of the same name" do
+    file_in_outer_group = File
+    example { expect(File).to eq ::File }
+    example { expect(file_in_outer_group).to be(::File) }
+
+    describe "File" do
+      file_in_inner_group = File
+      example { expect(File).to eq ::File }
+      example { expect(file_in_inner_group).to be(::File) }
+    end
+  end
+end
