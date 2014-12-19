1febdccaa13b7479c6d0f807cd01ff82d62963e1
Myron Marston <myron.marston@gmail.com>
2014-12-18 22:35:48 -0800

    Update benchmark allocations helper to support min count.
    This is useful when we have many groups because each group
    is a separate class.

--- benchmarks/allocations/helper.rb
+++ benchmarks/allocations/helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.expand_path("../../../lib", __FILE__)
 require 'rspec/core'
 require 'allocation_stats'
 
-def benchmark_allocations(burn: 1)
+def benchmark_allocations(burn: 1, min_allocations: 0)
   stats = AllocationStats.new(burn: burn).trace do
     yield
   end
@@ -13,5 +13,11 @@ def benchmark_allocations(burn: 1)
               [:class_plus]
             end
 
-  puts stats.allocations(alias_paths: true).group_by(*columns).from_pwd.sort_by_size.to_text
+  results = stats.allocations(alias_paths: true).group_by(*columns).from_pwd.sort_by_size.to_text
+  filtered = results.split("\n").select do |line|
+    count = line[/\s+(\d+)\z/, 1]
+    count.nil? || Integer(count) >= min_allocations
+  end.join("\n")
+
+  puts filtered
 end
