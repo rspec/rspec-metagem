aa7188fd84297ccbbb40c68afdadc628756488cc
Alexey Fedorov <waterlink000@gmail.com>
2015-01-28 09:48:33 +0200

    disable rubocop ClassLength for Compound matcher

--- lib/rspec/matchers/built_in/compound.rb
+++ lib/rspec/matchers/built_in/compound.rb
@@ -3,6 +3,7 @@ module RSpec
     module BuiltIn
       # @api private
       # Base class for `and` and `or` compound matchers.
+      # rubocop:disable ClassLength
       class Compound < BaseMatcher
         # @private
         attr_reader :matcher_1, :matcher_2, :evaluator
