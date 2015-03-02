929e21db945d59b32e4bc66415932bf1556d0f32
Jared Beck <jared@jaredbeck.com>
2015-03-02 11:16:10 -0500

    Make Pretty#underscore private

--- lib/rspec/matchers/pretty.rb
+++ lib/rspec/matchers/pretty.rb
@@ -10,17 +10,6 @@ module RSpec
         defined?(@name) ? @name : underscore(self.class.name.split("::").last)
       end
 
-      # @private
-      # Borrowed from ActiveSupport
-      def underscore(camel_cased_word)
-        word = camel_cased_word.to_s.dup
-        word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-        word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-        word.tr!("-", "_")
-        word.downcase!
-        word
-      end
-
     private
 
       # `{ :a => 5, :b => 2 }.inspect` produces:
@@ -34,6 +23,17 @@ module RSpec
       # This is idempotent and safe to run on a string multiple times.
       def improve_hash_formatting(inspect_string)
         inspect_string.gsub(/(\S)=>(\S)/, '\1 => \2')
+      end
+
+      # @private
+      # Borrowed from ActiveSupport.
+      def underscore(camel_cased_word)
+        word = camel_cased_word.to_s.dup
+        word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+        word.tr!("-", "_")
+        word.downcase!
+        word
       end
     end
   end
