b00d6bc384bf023001fb415c4668526709c7bf06
Myron Marston <myron.marston@gmail.com>
2015-04-20 10:12:01 -0700

    Improve our spec for minitest integration.

--- spec/rspec/expectations/minitest_integration_spec.rb
+++ spec/rspec/expectations/minitest_integration_spec.rb
@@ -10,12 +10,12 @@ module RSpec
     context "once required", :slow do
       include MinitestIntegration
 
-      it "includes itself in Minitest::Test" do
+      it "includes itself in Minitest::Test, and sets up our exceptions to be counted as assertion failures" do
         with_minitest_loaded do
           minitest_case = MiniTest::Test.allocate
-          sample_matchers.each do |sample_matcher|
-              expect(minitest_case).to respond_to(sample_matcher)
-          end
+          expect(minitest_case).to respond_to(*sample_matchers)
+
+          expect(RSpec::Expectations::ExpectationNotMetError).to be ::Minitest::Assertion
         end
       end
 
