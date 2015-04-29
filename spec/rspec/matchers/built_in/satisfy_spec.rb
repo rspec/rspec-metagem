98d36d762372a3ae84fe8a9056edca0dc63f8e93
ChrisArcand <chris@chrisarcand.com>
2015-04-28 23:48:24 -0400

    Updated spec and documentation for issue #719
    Expected results of code changes for issue #719, allowing the `satisfy`
    matcher to accept a custom description for improved failure messages.

--- spec/rspec/matchers/built_in/satisfy_spec.rb
+++ spec/rspec/matchers/built_in/satisfy_spec.rb
@@ -24,6 +24,30 @@ RSpec.describe "expect(...).to satisfy { block }" do
       end
     end.to fail_with("expected false to satisfy block")
   end
+
+  context "when a custom description is provided" do
+    it "describes itself" do
+      expect(satisfy("be awesome").description).to eq("be awesome")
+    end
+
+    it "passes if block returns true" do
+      expect(true).to satisfy("be true") { |val| val }
+      expect(true).to satisfy("be true") do |val|
+        val
+      end
+    end
+
+    it "fails with the custom description if block returns false" do
+      expect {
+        expect(false).to satisfy("be true") { |val| val }
+      }.to fail_with("expected false to be true")
+      expect do
+        expect(false).to satisfy("be true") do |val|
+          val
+        end
+      end.to fail_with("expected false to be true")
+    end
+  end
 end
 
 RSpec.describe "expect(...).not_to satisfy { block }" do
@@ -38,5 +62,20 @@ RSpec.describe "expect(...).not_to satisfy { block }" do
     expect {
       expect(true).not_to satisfy { |val| val }
     }.to fail_with("expected true not to satisfy block")
+  end
+
+  context "when a custom description is provided" do
+    it "passes if block returns false" do
+      expect(false).not_to satisfy("be true") { |val| val }
+      expect(false).not_to satisfy("be true") do |val|
+        val
+      end
+    end
+
+    it "fails with the custom description if block returns true" do
+      expect {
+        expect(true).not_to satisfy("be true") { |val| val }
+      }.to fail_with("expected true not to be true")
+    end
   end
 end
