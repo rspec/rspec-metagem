3bef84bf4cce96a4417a6550d39f53390ec81439
Sam Phippen <samphippen@googlemail.com>
2017-05-04 19:56:40 -0400

    Releases 3.6.0

--- lib/rspec/core/version.rb
+++ lib/rspec/core/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Core.
     module Version
       # Current version of RSpec Core, in semantic versioning format.
-      STRING = '3.6.0.beta2'
+      STRING = '3.6.0'
     end
   end
 end
