551c365197c8cae045967f872d3cd6ecf4c593a8
Sam Phippen <samphippen@googlemail.com>
2017-05-04 20:11:09 -0400

    Bump version to 3.7.0.pre

--- lib/rspec/core/version.rb
+++ lib/rspec/core/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Core.
     module Version
       # Current version of RSpec Core, in semantic versioning format.
-      STRING = '3.6.0'
+      STRING = '3.7.0.pre'
     end
   end
 end
