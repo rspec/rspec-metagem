96450f8468baf601b9e5c00c08bcac18013426ff
Myron Marston <myron.marston@gmail.com>
2018-08-04 13:05:24 -0700

    Bump version to 3.9.0.pre.

--- lib/rspec/core/version.rb
+++ lib/rspec/core/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Core.
     module Version
       # Current version of RSpec Core, in semantic versioning format.
-      STRING = '3.8.0'
+      STRING = '3.9.0.pre'
     end
   end
 end
